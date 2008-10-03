f015d5907c0ddf5ab0931ac8554ba9e23317a3ae
andrew <andrew@31352e9c-24da-da11-96a7-0090fb0792c6>
2008-10-03 19:53:50 +0000

    Make test_coverage module output text and html coverage reports
    git-svn-id: http://svn.dev/cpx/cpxerl@2193 31352e9c-24da-da11-96a7-0090fb0792c6

--- src/test_coverage.erl
+++ src/test_coverage.erl
@@ -12,4 +12,5 @@ start(Module) ->
 	cover:start(),
 	cover:compile_beam(string:concat("ebin/", atom_to_list(Module))),
 	apply(Module, test, []),
-	cover:analyse_to_file(Module).
+	cover:analyse_to_file(Module, string:concat(string:concat("coverage/", atom_to_list(Module)), ".txt")),
+	cover:analyse_to_file(Module, string:concat(string:concat("coverage/", atom_to_list(Module)), ".html"), [html]).

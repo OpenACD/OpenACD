548ce21667a3f3f70fa3527cb23849ef5884ba8b
andrew <andrew@31352e9c-24da-da11-96a7-0090fb0792c6>
2008-10-13 17:45:24 +0000

    Totally forgot to commit this before - * improved util:string_split to make it a bit more consistant with ruby's implementation * added tests
    git-svn-id: http://svn.dev/cpx/cpxerl@2226 31352e9c-24da-da11-96a7-0090fb0792c6

--- src/util.erl
+++ src/util.erl
@@ -6,8 +6,14 @@
 
 -export([string_split/3, string_split/2]).
 
+string_split("", _Separator, _SplitCount) ->
+	[];
+string_split(String, "", 1) ->
+	[String];
 string_split(String, _Separator, 1) ->
 	[String];
+string_split(String, "", SplitCount) ->
+	[string:substr(String, 1, 1) | string_split(string:substr(String, 2), "", SplitCount - 1)];
 string_split(String, Separator, SplitCount) ->
 	case string:str(String, Separator) of
 		0 ->
@@ -16,6 +22,10 @@ string_split(String, Separator, SplitCount) ->
 			[string:substr(String, 1, Index - 1) | string_split(string:substr(String, Index + string:len(Separator)), Separator, SplitCount - 1)]
 	end.
 
+string_split("", _Separator) ->
+	[];
+string_split(String, "") ->
+	[string:substr(String, 1, 1) | string_split(string:substr(String, 2), "")];
 string_split(String, Separator) ->
 	case string:str(String, Separator) of
 		0 ->
@@ -23,3 +33,22 @@ string_split(String, Separator) ->
 		Index ->
 			[string:substr(String, 1, Index - 1) | string_split(string:substr(String, Index + string:len(Separator)), Separator)]
 	end.
+
+-ifdef(EUNIT).
+split_empty_string_test() ->
+	?assertEqual([] , string_split("", " ")).
+split_by_empty_string_test() ->
+	?assertEqual(["f", "o", "o"], string_split("foo", "")).
+split_by_empty_string_with_limit_test() ->
+	?assertEqual(["f", "oo"], string_split("foo", "", 2)).
+split_string_test() ->
+	?assertEqual(["hello", "world", ":)"], string_split("hello world :)", " ")).
+split_string_with_limit_test() ->
+	?assertEqual(["hello", "world :)"], string_split("hello world :)", " ", 2)).
+split_empty_string_with_limit_test() ->
+	?assertEqual([], string_split("", " ", 3)).
+split_by_nonexistant_string_test() ->
+	?assertEqual(["Hello world"], string_split("Hello world", "batman")).
+split_by_nonexistant_string_with_limit_test() ->
+	?assertEqual(["Hello world"], string_split("Hello world", "batman", 3)).
+-endif.

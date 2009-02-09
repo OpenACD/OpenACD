37784ec0d295316e961e85b5552ed1099f743430
Andrew Thompson <andrew@hijacked.us>
2009-02-09 11:41:34 -0500

    Re-work the gen-server test header to be a test generator

--- include/gen_server_test.hrl
+++ include/gen_server_test.hrl
@@ -1,23 +1,49 @@
 
+%% Macro to test generic gen_server behaviour. MYSERVERFUNC must be
+%% defined as a fun which returns {pid(), fun()} or {atom(), fun()}.
+%% The first element is the pid or registered name of the gen_server
+%% and the second id the function to be run to stop the gen_server
+%% after the test has run.
+
 -ifdef(MYSERVERFUNC).
-gen_server_test() ->
+gen_server_test_() ->
+	% start
 	{Server, StopFunc} = ?MYSERVERFUNC(),
-	?assertEqual({unknown_call, garbage}, gen_server:call(Server, garbage)),
-	?assertEqual(ok, gen_server:cast(Server, garbage)),
-	% execute the code_change handler
+
 	case Server of
 		Pid when is_pid(Pid) ->
-			sys:suspend(Pid),
-			?assertEqual(ok, sys:change_code(Pid, "", ?MODULE, "")),
-			sys:resume(Pid),
-			Pid ! garbage;
+			ok;
 		Server ->
 			Pid = whereis(Server),
-			sys:suspend(Pid),
-			?assertEqual(ok, sys:change_code(Pid, "", ?MODULE, "")),
-			sys:resume(Pid),
-			Pid ! garbage
+			ok
 	end,
-	StopFunc().
+
+	[{ "handle_call with garbage value",
+		fun() -> ?assertEqual({unknown_call, garbage}, gen_server:call(Server, garbage)) end
+	},
+
+	{ "handle_cast with garbage value",
+		fun() -> ?assertEqual(ok, gen_server:cast(Server, garbage)) end
+	},
+
+
+	{ "code_change",
+		fun() ->
+			?assertEqual(ok, sys:suspend(Pid)),
+			?assertEqual(ok, sys:change_code(Pid, "", ?MODULE, "")),
+			?assertEqual(ok, sys:resume(Pid))
+		end
+	},
+
+	{ "handle info with garbage value",
+		fun() -> Pid ! garbage end
+	},
+
+	{ "testing stop",
+		fun() ->
+			?assertMatch(ok, StopFunc()),
+			?assertMatch(false, is_process_alive(Pid))
+		end
+	}].
 
 -endif.

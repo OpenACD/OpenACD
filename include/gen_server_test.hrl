124ce18b8c3f71be7d88d1a66c9a31c11d4cdcdf
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-02-25 15:34:47 -0500

    Chnaged license to CPAL

--- include/gen_server_test.hrl
+++ include/gen_server_test.hrl
@@ -1,4 +1,33 @@
+%%	The contents of this file are subject to the Common Public Attribution
+%%	License Version 1.0 (the “License”); you may not use this file except
+%%	in compliance with the License. You may obtain a copy of the License at
+%%	http://opensource.org/licenses/cpal_1.0. The License is based on the
+%%	Mozilla Public License Version 1.1 but Sections 14 and 15 have been
+%%	added to cover use of software over a computer network and provide for
+%%	limited attribution for the Original Developer. In addition, Exhibit A
+%%	has been modified to be consistent with Exhibit B.
+%%
+%%	Software distributed under the License is distributed on an “AS IS”
+%%	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%%	License for the specific language governing rights and limitations
+%%	under the License.
+%%
+%%	The Original Code is Spice Telephony.
+%%
+%%	The Initial Developers of the Original Code is 
+%%	Andrew Thompson and Micah Warren.
+%%
+%%	All portions of the code written by the Initial Developers are Copyright
+%%	(c) 2008-2009 SpiceCSM.
+%%	All Rights Reserved.
+%%
+%%	Contributor(s):
+%%
+%%	Andrew Thompson <athompson at spicecsm dot com>
+%%	Micah Warren <mwarren at spicecsm dot com>
+%%
 
+%% @hidden
 %% Macro to test generic gen_server behaviour. MYSERVERFUNC must be
 %% defined as a fun which returns {pid(), fun()} or {atom(), fun()}.
 %% The first element is the pid or registered name of the gen_server
--- src/cpx_supervisor.erl
+++ src/cpx_supervisor.erl
@@ -1,25 +1,31 @@
-%% The contents of this file are subject to the Mozilla Public License
-%% Version 1.1 (the "License"); you may not use this file except in
-%% compliance with the License. You may obtain a copy of the License at
-%% http://www.mozilla.org/MPL/
-%% 
-%% Software distributed under the License is distributed on an "AS IS"
-%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-%% License for the specific language governing rights and limitations
-%% under the License.
-%% 
-%% The Original Code is Spice Telphony.
-%% 
-%% The Initial Developer of the Original Code is 
-%% Andrew Thompson and Micah Warren.
-%% Portions created by the Initial Developers are Copyright (C) 
-%% SpiceCSM. All Rights Reserved.
-
-%% Contributor(s): 
-
-%% Andrew Thompson <athompson at spicecsm dot com>
-%% Micah Warren <mwarren at spicecsm dot com>
-%% 
+%%	The contents of this file are subject to the Common Public Attribution
+%%	License Version 1.0 (the “License”); you may not use this file except
+%%	in compliance with the License. You may obtain a copy of the License at
+%%	http://opensource.org/licenses/cpal_1.0. The License is based on the
+%%	Mozilla Public License Version 1.1 but Sections 14 and 15 have been
+%%	added to cover use of software over a computer network and provide for
+%%	limited attribution for the Original Developer. In addition, Exhibit A
+%%	has been modified to be consistent with Exhibit B.
+%%
+%%	Software distributed under the License is distributed on an “AS IS”
+%%	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%%	License for the specific language governing rights and limitations
+%%	under the License.
+%%
+%%	The Original Code is Spice Telephony.
+%%
+%%	The Initial Developers of the Original Code is 
+%%	Andrew Thompson and Micah Warren.
+%%
+%%	All portions of the code written by the Initial Developers are Copyright
+%%	(c) 2008-2009 SpiceCSM.
+%%	All Rights Reserved.
+%%
+%%	Contributor(s):
+%%
+%%	Andrew Thompson <athompson at spicecsm dot com>
+%%	Micah Warren <mwarren at spicecsm dot com>
+%%
 
 %% @doc The top-most supervisor of the cpx system.  This is responsible for starting and monitoring the primary supervisors
 %% as well as any additional modules that are configured.  

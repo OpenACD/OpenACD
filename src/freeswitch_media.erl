9111b8624a0eda1e5069e312a269fd8a8a77048c
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-03-16 19:33:53 -0400

    handle_info for (soon to be) call_hangup message.

--- src/freeswitch_media.erl
+++ src/freeswitch_media.erl
@@ -311,6 +311,9 @@ handle_info({bgerror, "-ERR USER_BUSY\n"}, State) ->
 handle_info({bgerror, Reply}, State) ->
 	?CONSOLE("unhandled bgerror: ~p", [Reply]),
 	{noreply, State};
+handle_info(call_hangup, State) ->
+	?CONSOLE("Call hangup info, terminating", []),
+	{stop, normal, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 

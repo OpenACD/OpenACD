962788b50ae57fd9c4ed93af502a7600509fcd88
Andrew Thompson <andrew@freecpx.dev>
2009-04-23 12:48:42 -0400

    Workaround for ref() type

--- src/agent_web_connection.erl
+++ src/agent_web_connection.erl
@@ -60,6 +60,10 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
+-ifdef(R13B).
+-type ref() :: reference().
+-endif.
+
 -record(state, {
 	salt :: any(),
 	ref :: ref() | 'undefined',

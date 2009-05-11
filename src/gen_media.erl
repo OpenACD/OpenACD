895dbebaac1f0b66e19a59bb45023823abb1d3c8
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-05-11 13:48:26 -0400

    Correct behaviour_info declaration and implementation

--- src/gen_media.erl
+++ src/gen_media.erl
@@ -1,26 +1,63 @@
-%%%-------------------------------------------------------------------
-%%% File          : gen_media.erl
-%%% Author        : Micah Warren
-%%% Organization  : SpiceCSM
-%%% Project       : spice-telephony
-%%% Description   : 
-%%%
-%%% Created       :  5/11/09
-%%%-------------------------------------------------------------------
+%%	The contents of this file are subject to the Common Public Attribution
+%%	License Version 1.0 (the “License”); you may not use this file except
+%%	in compliance with the License. You may obtain a copy of the License at
+%%	http://opensource.org/licenses/cpal_1.0. The License is based on the
+%%	Mozilla Public License Version 1.1 but Sections 14 and 15 have been
+%%	added to cover use of software over a computer network and provide for
+%%	limited attribution for the Original Developer. In addition, Exhibit A
+%%	has been modified to be consistent with Exhibit B.
+%%
+%%	Software distributed under the License is distributed on an “AS IS”
+%%	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%%	License for the specific language governing rights and limitations
+%%	under the License.
+%%
+%%	The Original Code is Spice Telephony.
+%%
+%%	The Initial Developers of the Original Code is 
+%%	Andrew Thompson and Micah Warren.
+%%
+%%	All portions of the code written by the Initial Developers are Copyright
+%%	(c) 2008-2009 SpiceCSM.
+%%	All Rights Reserved.
+%%
+%%	Contributor(s):
+%%
+%%	Andrew Thompson <athompson at spicecsm dot com>
+%%	Micah Warren <mwarren at spicecsm dot com>
+%%
+
 -module(gen_media).
 -author(micahw).
 
 -behaviour(gen_server).
 
+-include("log.hrl").
+-include("call.hrl").
+
+-ifdef(EUNIT).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% API
 -export([
-	behaviour_info/1
-	start_link/0
+	behaviour_info/1,
+	start_link/2
 ]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
+
+%% gen_media api
+-export([
+	ring/4,
+	get_call/1,
+	voicemail/1,
+	announce/2,
+	stop_ringing/1,
+	oncall/1
+]).
 
 -record(state, {
 	callback,
@@ -29,10 +66,17 @@
 	agent_pid
 }).
 
+-type(state() :: #state{}).
+-define(GEN_SERVER, true).
+-include("gen_spec.hrl").
+
 -define(MEDIA_ACTIONS, [ring_agent, get_call, start_cook, voicemail, announce, stop_cook]).
 
+-spec(behaviour_info/1 :: 
+	(Info :: 'callbacks') -> [{atom(), non_neg_integer()}];
+	(Info :: any()) -> 'undefined').
 behaviour_info(callbacks) ->
-	GS = gen_server:behavior_info(callbacks),
+	GS = gen_server:behaviour_info(callbacks),
 	lists:append([{handle_ring, 3}, {handle_answer, 3}, {handle_voicemail, 1}, {handle_annouce, 2}], GS);
 behaviour_info(_Other) ->
     undefined.
@@ -102,10 +146,10 @@ init([Callback, Args]) ->
 
 handle_call('$gen_media_get_call', From, State) ->
 	{reply, State#state.callrec, State};
-handle_call({'$gen_media_ring', Agent, Qcall, Timeout}, From, State) ->
+handle_call({'$gen_media_ring', Agent, QCall, Timeout}, From, #state{callrec = Call} = State) ->
 	case agent:set_state(Agent, ringing, Call#call{cook=QCall#queued_call.cook}) of
 		ok ->
-			case apply(State#state.callback, handle_ring, [Agent]) of
+			case apply(State#state.callback, handle_ring, [Agent, State#state.callrec, State#state.substate]) of
 				{ok, Substate} ->
 					{reply, ok, State#state{substate = Substate, agent_pid = Agent}};
 				{invalid, Substate} ->
@@ -113,24 +157,25 @@ handle_call({'$gen_media_ring', Agent, Qcall, Timeout}, From, State) ->
 			end;
 		Else ->
 			?INFO("Agent ringing response:  ~p", [Else]),
-			{reply, invalid, State};
+			{reply, invalid, State}
+	end;
 handle_call({'$gen_media_annouce', Annouce}, From, State) ->
-	{ok, Substate} = apply(State#state.callback, handle_annouce, [Annouce]),
+	{ok, Substate} = apply(State#state.callback, handle_annouce, [Annouce, State]),
 	{reply, ok, State#state{substate = Substate}};
 handle_call('$gen_media_voicemail', From, State) ->
 	{ok, Substate} = apply(State#state.callback, handle_voicemail, []),
 	{reply, ok, State#state{substate = Substate}};
-handle_call(Request, From, State) ->
-	case apply(State#state.callback, handle_call, [Request, From, State#state.substate]) of
+handle_call(Request, From, #state{callback = Callback} = State) ->
+	case Callback:handle_call(Request, From, State#state.substate) of
 		{reply, Reply, NewState} ->
 			{reply, Reply, State#state{substate = NewState}};
-		{reply, Reply, Newstate, Timeout} ->
+		{reply, Reply, Newstate, Timeout} when is_integer(Timeout) ->
 			{reply, Reply, State#state{substate = Newstate}, Timeout};
 		{reply, Reply, NewState, hibernate} ->
-			{reply, Reply, State#state{substate = NewState}, hibernate} ->
+			{reply, Reply, State#state{substate = NewState}, hibernate};
 		{noreply, NewState} ->
 			{noreply, State#state{substate = NewState}};
-		{noreply, NewState, Timeout} ->
+		{noreply, NewState, Timeout} when is_integer(Timeout) ->
 			{noreply, State#state{substate = NewState}, Timeout};
 		{noreply, NewState, hibernate} ->
 			{noreply, State#state{substate = NewState}, hibernate};
@@ -150,7 +195,7 @@ handle_cast('$gen_media_agent_oncall', State) ->
 			?WARNING("No agent to set state to",[]),
 			{noreply, State};
 		Apid when is_pid(Apid) ->
-			case apply(State#state.callback, handle_answer, [State#state.callrec, Apid, State#state.substate]) of
+			case apply(State#state.callback, handle_answer, [Apid, State#state.callrec, State#state.substate]) of
 				{ok, Newstate} ->
 					agent:set_state(State#state.agent_pid, oncall, State#state.callrec),
 					{noreply, State#state{substate = Newstate}};
@@ -163,7 +208,7 @@ handle_cast(Msg, State) ->
 	case apply(State#state.callback, handle_cast, [Msg, State#state.substate]) of
 		{noreply, NewState} ->
 			{noreply, State#state{substate = NewState}};
-		{noreply, NewState, Timeout} ->
+		{noreply, NewState, Timeout} when is_integer(Timeout) ->
 			{noreply, State#state{substate = NewState}, Timeout};
 		{noreply, NewState, hibernate} ->
 			{noreply, State#state{substate = NewState}, hibernate};
@@ -178,7 +223,7 @@ handle_info(Info, State) ->
 	case apply(State#state.callback, handle_info, [Info, State#state.substate]) of
 		{noreply, NewState} ->
 			{noreply, State#state{substate = NewState}};
-		{noreply, NewState,  Timeout} ->
+		{noreply, NewState,  Timeout} when is_integer(Timeout) ->
 			{noreply, State#state{substate = NewState}, Timeout};
 		{noreply, NewState, hibernate} ->
 			{noreply, State#state{substate = NewState}, hibernate};
@@ -202,3 +247,10 @@ code_change(OldVsn, State, Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+
+
+-ifdef(EUNIT).
+
+
+
+-endif.

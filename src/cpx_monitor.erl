e3a64b99ac4076280b719396965266153b5b6a2b
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-06-30 13:11:55 -0400

    Converted to gen_leader for better node monitoring.

--- src/cpx_monitor.erl
+++ src/cpx_monitor.erl
@@ -33,7 +33,7 @@
 -module(cpx_monitor).
 -author(micahw).
 
--behaviour(gen_server).
+-behaviour(gen_leader).
 
 -ifdef(EUNIT).
 -include_lib("eunit/include/eunit.hrl").
@@ -57,27 +57,41 @@
 	]).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-	 terminate/2, code_change/3]).
+-export([init/1, 
+	elected/3,
+	surrendered/3,
+	handle_DOWN/3,
+	handle_leader_call/4,
+	handle_leader_cast/3,
+	from_leader/3,
+	handle_call/4, 
+	handle_cast/3, 
+	handle_info/2,
+	terminate/2, 
+	code_change/4]).
 
 -record(state, {
 	nodes = [] :: [atom()],
 	monitoring = [] :: [{atom(), any()}],
-	down = [] :: [atom()]
+	down = [] :: [atom()],
+	auto_restart_mnesia = true :: 'false' | 'false',
+	merge_status = []
 }).
 
 %%====================================================================
 %% API
 %%====================================================================
 	
-start_link(Args) when is_list(Args) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+start_link(Args) ->
+	Nodes = proplists:get_all_values(nodes, Args),
+    gen_leader:start_link(?MODULE, Nodes, [], ?MODULE, Args, []).
 
 start(Args) ->
-	gen_server:start({local, ?MODULE}, ?MODULE, Args, []).
+	Nodes = proplists:get_all_values(nodes, Args),
+	gen_leader:start(?MODULE, Nodes, [], ?MODULE, Args, []).
 	
 stop() ->
-	gen_server:call(?MODULE, stop).
+	gen_leader:call(?MODULE, stop).
 
 %%====================================================================
 %% gen_server callbacks
@@ -87,20 +101,55 @@ init(Args) when is_list(Args) ->
 	Nodes = proplists:get_all_values(nodes, Args),
 	Mons = Nodes,
 	lists:foreach(fun(N) -> monitor_node(N, true) end, Mons),
-    {ok, #state{nodes = Nodes, monitoring = Mons}}.
+    {ok, #state{
+		nodes = Nodes, 
+		monitoring = Mons,
+		auto_restart_mnesia = proplists:get_value(auto_restart_mnesia, Args, true)
+	}}.
+
+
+%% @hidden
+elected(State, Election, Node) -> 
+	?INFO("elected by ~w", [Node]),
+	{ok, ok, State}.
+	
+%% @hidden
+%% TODO what about an agent started at both places?
+surrendered(State, _LeaderState, _Election) -> 
+	{ok, State}.
+	
+%% @hidden
+handle_DOWN(Node, State, _Election) -> 
+	{ok, State}.
+
+%% @hidden
+handle_leader_call(Message, From, State, _Election) ->
+	?WARNING("received unexpected leader_call ~p from ~p", [Message, From]),
+	{reply, ok, State}.
+
+%% @hidden
+handle_leader_cast(Message, State, Election) ->
+	?WARNING("received unexpected leader_cast ~p", [Message]),
+	{noreply, State}.
+
+%% @hidden
+from_leader(_Msg, State, _Election) -> 
+	?DEBUG("Stub from leader.", []),
+	{ok, State}.
 
 %%--------------------------------------------------------------------
 %% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
-handle_call(stop, _From, State) ->
+%%--------------------------------------------------------------------
+handle_call(stop, _From, State, Election) ->
 	{stop, normal, ok, State};
-handle_call(_Request, _From, State) ->
+handle_call(_Request, _From, State, Election) ->
     Reply = ok,
     {reply, Reply, State}.
 
 %%--------------------------------------------------------------------
 %% Function: handle_cast(Msg, State) -> {noreply, State} |
 %%--------------------------------------------------------------------
-handle_cast({recover, Node}, State) ->
+handle_cast({recover, Node}, State, _Election) ->
 	case lists:member(Node, State#state.down) of
 		false ->
 			% just chill
@@ -111,7 +160,7 @@ handle_cast({recover, Node}, State) ->
 			Newmon = [Node | State#state.monitoring],
 			{noreply, State#state{down = Newdown, monitoring = Newmon}}
 	end;
-handle_cast(_Msg, State) ->
+handle_cast(_Request, State, _Election) -> 
     {noreply, State}.
 
 %%--------------------------------------------------------------------
@@ -149,7 +198,7 @@ terminate(_Reason, _State) ->
 %%--------------------------------------------------------------------
 %% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
 %%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
+code_change(_OldVsn, State, _Election, _Extra) ->
     {ok, State}.
 
 %%--------------------------------------------------------------------
@@ -199,14 +248,14 @@ state_test_() ->
 	{"recovery message",
 	fun() ->
 		State = #state{down = [node]},
-		{noreply, Newstate} = handle_cast({recover, node}, State),
+		{noreply, Newstate} = handle_cast({recover, node}, State, {}),
 		Expected = #state{monitoring = [node], down = []},
 		?assertEqual(Expected, Newstate)
 	end},
 	{"recovery about a node that isn't down",
 	fun() ->
 		State = #state{monitoring = [node]},
-		{noreply, Newstate} = handle_cast({recover, node}, State),
+		{noreply, Newstate} = handle_cast({recover, node}, State, {}),
 		?assertEqual(State, Newstate)
 	end}].
 	

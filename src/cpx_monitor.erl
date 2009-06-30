e39c251781a7dbf951bb2a1ffd730554e7e8ba85
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-06-30 12:35:25 -0400

    Syntax fixes and test creations.

--- src/cpx_monitor.erl
+++ src/cpx_monitor.erl
@@ -118,7 +118,7 @@ handle_cast(_Msg, State) ->
 %% Function: handle_info(Info, State) -> {noreply, State} |
 %%--------------------------------------------------------------------
 handle_info({nodedown, Node}, State) ->
-	case lists:member(Node, State#state.monitoring of
+	case lists:member(Node, State#state.monitoring) of
 		true ->
 			%monitor_node(Node, false),
 			?ALERT("Node ~w is detected as being down!", [Node]),
@@ -127,13 +127,16 @@ handle_info({nodedown, Node}, State) ->
 			Sfun = fun() ->
 				check_loop(Node)
 			end,
-			Pid = spawn(Sfun),
+			Pid = spawn_link(Sfun),
 			timer:send_after(10000, Pid, check),
 			{noreply, State#state{down = Newdown, monitoring = Newmon}};
 		false ->
 			% just chill
 			{noreply, State}
 	end;
+handle_info({'EXIT', From, Reason}, State) ->
+	?INFO("~p said it died due to ~p.", [From, Reason]),
+	{noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -175,10 +178,38 @@ check_loop(Node) ->
 			check_loop(Node)
 	end.
 
-
-
 -ifdef(EUNIT).
 
+state_test_() ->
+	[{"node down message recieved",
+	fun() ->
+		State = #state{
+			monitoring = [node]
+		},
+		{noreply, Newstate} = handle_info({nodedown, node}, State),
+		Expect = #state{down = [node], monitoring = []},
+		?assertEqual(Expect, Newstate)
+	end},
+	{"node down message about a node we aren't monitoring",
+	fun() ->
+		State = #state{down = [node]},
+		{noreply, Newstate} = handle_info({nodedown, node}, State),
+		?assertEqual(State, Newstate)
+	end},
+	{"recovery message",
+	fun() ->
+		State = #state{down = [node]},
+		{noreply, Newstate} = handle_cast({recover, node}, State),
+		Expected = #state{monitoring = [node], down = []},
+		?assertEqual(Expected, Newstate)
+	end},
+	{"recovery about a node that isn't down",
+	fun() ->
+		State = #state{monitoring = [node]},
+		{noreply, Newstate} = handle_cast({recover, node}, State),
+		?assertEqual(State, Newstate)
+	end}].
+	
 
 -endif.
 

4e4d9284bb27b242273b89602de3e1d622bf375f
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-06-30 12:07:59 -0400

    Extended and solidified cpx_monitor's role/function

--- src/cpx_monitor.erl
+++ src/cpx_monitor.erl
@@ -27,15 +27,13 @@
 %%	Micah Warren <mwarren at spicecsm dot com>
 %%
 
-%% @doc Stat tracker callback module.  Similar to the cdr handler, the monitor
-%% tracks changes to the system, and recalculates the aggregate values about
-%% every 5 seconds based on the tracked changes.
-%%
+%% @doc Stat tracker and monitoring module.  A combination of the cdr handler
+%% and cpx_supervisor.  This handles health stats, and non-process monitoring.
 
 -module(cpx_monitor).
 -author(micahw).
 
--behaviour(gen_event).
+-behaviour(gen_server).
 
 -ifdef(EUNIT).
 -include_lib("eunit/include/eunit.hrl").
@@ -53,112 +51,100 @@
 
 %% API
 -export([
-	start/0,
-	start_link/0, 
-	add_handler/1
-]).
+	start_link/1,
+	start/1,
+	stop/0
+	]).
 
-%% gen_event callbacks
--export([init/1, handle_event/2, handle_call/2, 
-	 handle_info/2, terminate/2, code_change/3]).
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
 
 -record(state, {
-	debug = false,
-	regen_timer,
-	data_cache = {0, []} :: system_health()
+	nodes = [] :: [atom()],
+	monitoring = [] :: [{atom(), any()}],
+	down = [] :: [atom()]
 }).
 
--define(REGEN_INTERVAL, 5000).
--define(DEBUG_OUTPUT, {10, [
-	{'unhealthy@testplace', {0, [
-		{"unhealthyqueuegroup", {0, 
-			[{"healthyqueue", {100, [
-				{"healthycall1", {100}},
-				{"healthycall2", {90}}
-			]}},
-			{"unhealthyqueue", {0, [
-				{"unhealthycall1", {0}},
-				{"unhealthycall2", {20}}
-			]}}
-		]}}
-	]}},
-	{'healthy@testplace', {100, [
-		{"healthyqueuegroup", {80, 
-			[{"healthyqueue", {90, [
-				{"healthycall1", {80}},
-				{"healythcall2", {70}}
-			]}}
-		]}}
-	]}}
-]}).
+%%====================================================================
+%% API
+%%====================================================================
 	
+start_link(Args) when is_list(Args) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+
+start(Args) ->
+	gen_server:start({local, ?MODULE}, ?MODULE, Args, []).
+	
+stop() ->
+	gen_server:call(?MODULE, stop).
 
 %%====================================================================
-%% gen_event callbacks
+%% gen_server callbacks
 %%====================================================================
-start() ->
-	gen_event:start({local, ?MODULE}).
-
-start_link() ->
-    gen_event:start_link({local, ?MODULE}). 
-
-debug() ->
-	start(),
-	add_handler([{debug, true}]).
+init(Args) when is_list(Args) ->
+	process_flag(trap_exit, true),
+	Nodes = proplists:get_all_values(nodes, Args),
+	Mons = Nodes,
+	lists:foreach(fun(N) -> monitor_node(N, true) end, Mons),
+    {ok, #state{nodes = Nodes, monitoring = Mons}}.
 
 %%--------------------------------------------------------------------
-%% Function: add_handler() -> ok | {'EXIT',Reason} | term()
-%%--------------------------------------------------------------------
-add_handler(Opts) ->
-    gen_event:add_handler(?MODULE, ?MODULE, [Opts]).
-
-%%====================================================================
-%% gen_event callbacks
-%%====================================================================
-%%--------------------------------------------------------------------
-%% Function: init(Args) -> {ok, State}
-%%--------------------------------------------------------------------
-init(Opts) ->
-	Debugs = proplists:get_value(debug, Opts, false),
-	{ok, Tref} = timer:send_after(?REGEN_INTERVAL, regen_data),
-    {ok, #state{debug = Debugs, regen_timer = Tref}}.
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+handle_call(stop, _From, State) ->
+	{stop, normal, ok, State};
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
 
 %%--------------------------------------------------------------------
-%% handle_event(Event, State) -> {ok, State} |
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
 %%--------------------------------------------------------------------
-handle_event(Event, State) ->
-	?INFO("Handilning event ~p", [Event]),
-	%% TODO actually implement the stats tracking
-    {ok, State}.
+handle_cast({recover, Node}, State) ->
+	case lists:member(Node, State#state.down) of
+		false ->
+			% just chill
+			{noreply, State};
+		true ->
+			?INFO("~w appears to have recovered.", [Node]),
+			Newdown = lists:delete(Node, State#state.down),
+			Newmon = [Node | State#state.monitoring],
+			{noreply, State#state{down = Newdown, monitoring = Newmon}}
+	end;
+handle_cast(_Msg, State) ->
+    {noreply, State}.
 
 %%--------------------------------------------------------------------
-%% handle_call(Request, State) -> {ok, Reply, State} |
+%% Function: handle_info(Info, State) -> {noreply, State} |
 %%--------------------------------------------------------------------
-
-handle_call(Request, State) ->
-	?DEBUG("Handling request ~p", [Request]),
-	{ok, ok, State}.
-
-%%--------------------------------------------------------------------
-%% handle_info(Info, State) -> {ok, State} |
-%%--------------------------------------------------------------------
-handle_info(regen_data, State) ->
-	?DEBUG("regenerating data cache", []),
-	% Again, this should acutally do stuff.  For now, just recreate the timer.
-	{ok, Tref} = timer:send_after(?REGEN_INTERVAL, regen_data),
-	{ok, State#state{regen_timer = Tref}};
+handle_info({nodedown, Node}, State) ->
+	case lists:member(Node, State#state.monitoring of
+		true ->
+			%monitor_node(Node, false),
+			?ALERT("Node ~w is detected as being down!", [Node]),
+			Newdown = [Node | State#state.down],
+			Newmon = lists:delete(Node, State#state.monitoring),
+			Sfun = fun() ->
+				check_loop(Node)
+			end,
+			Pid = spawn(Sfun),
+			timer:send_after(10000, Pid, check),
+			{noreply, State#state{down = Newdown, monitoring = Newmon}};
+		false ->
+			% just chill
+			{noreply, State}
+	end;
 handle_info(_Info, State) ->
-    {ok, State}.
+    {noreply, State}.
 
 %%--------------------------------------------------------------------
 %% Function: terminate(Reason, State) -> void()
 %%--------------------------------------------------------------------
-terminate(_Reason, #state{regen_timer = Tref} = State) ->
-	timer:cancel(Tref),
+terminate(_Reason, _State) ->
     ok.
 
 %%--------------------------------------------------------------------
-%% Function: code_change(OldVsn, State, Extra) -> {ok, NewState} 
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
 %%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
@@ -166,3 +152,33 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+
+recover(Node) ->
+	gen_server:cast(?MODULE, {recover, Node}).
+
+recovery_watch(Node) ->
+	monitor_node(Node, false),
+	receive
+		{nodedown, Node} ->
+			timer:send_after(1000, check)
+	after 100 ->
+		recover(Node),
+		ok
+	end,
+	check_loop(Node).
+
+check_loop(Node) ->
+	receive
+		check ->
+			recovery_watch(Node); 
+		_Otherwise -> 
+			check_loop(Node)
+	end.
+
+
+
+-ifdef(EUNIT).
+
+
+-endif.
+

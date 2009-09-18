5e196818038c24eb45e207dea82ce9b143d76ea7
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-09-18 14:02:49 -0400

    Forgot to include the actual js for the frontend...

---
+++ www/admin/spice/clients.js
@@ -0,0 +1,70 @@
+dojo.provide("spice.clients");
+
+clients = function(){
+	return {};
+}
+
+clients.store = null;
+
+clients.init = function(){
+	clients.store = new dojo.data.ItemFileWriteStore({
+		url:"/clients/getClients",
+		hierarchical:false,
+		typeMap:{
+			"json":function(obj){
+				return obj
+			}
+		}
+	});
+	clients.store._saveCustom = function(savecomplete){
+		var changeset = mailMappings._pending;
+		var updates = [];
+		for(var i in changeset._modifiedItems){
+			var item = null;
+			if(mailMappings._itemsByIdentity){
+				item = mailMappings._itemsByIdentity[i];
+			}
+			else{
+				item = mailMappings._arrayOfAllItems[i];
+			}
+			updates.push(item);
+		}
+		savecomplete();
+	}
+}	
+
+clients.setDefault = function(inconf){
+	var defaultConf = {
+		url_pop: "http://www.google.com/#q=#{label}"
+	};
+	
+	var conf = dojo.mixin(defaultConf, inconf);
+		
+	dojo.xhrPost({
+		url:"/clients/setDefault",
+		handleAs:'json',
+		content:conf,
+		load:function(res){
+			if(! res.success){
+				console.log(["client default set failure", res]);
+				return;
+			}
+			
+			clients.store.fetch({
+				query:{
+					label:""
+				},
+				onLoad:function(items){
+					var item = items[0];
+					for(var i in conf){
+						clients.store.setValue(item, i, conf[i]);
+					}
+					clients.store.save();
+				}
+			});
+		},
+		error:function(res){
+			console.log(["client default set error", res])
+		}
+	});
+}

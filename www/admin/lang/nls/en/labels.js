ceb8ad10b32f6c35423ce7846d54c5b6f13dd6a8
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-09-21 14:20:43 -0400

    Added to the help text

--- www/admin/lang/nls/en/labels.js
+++ www/admin/lang/nls/en/labels.js
@@ -2,7 +2,7 @@
 	CLIENTS:  "Clients",
 	DEFAULTS:  "Defaults",
 	URL_POP:  "Url pop",
-	URL_POP_EXPLANATION: "The url pop uses ruby-style interpoation for variables.  Available variables are #{label}, #{tennant}, #{brand}, and #{combo_id}.",
+	URL_POP_EXPLANATION: "The url pop uses ruby-style interpoation for variables.  Available variables are #{label}, #{tennant}, #{brand}, and #{combo_id}.  Http should be prepended in front of the url.",
 	USERNAME: "Username",
 	PASSWORD: "Password",
 	LOGIN: "Login",

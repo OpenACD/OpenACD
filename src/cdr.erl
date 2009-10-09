28170a4cb82df98ab17df6c6520dadcc7cadbe04
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-10-08 20:31:48 -0400

    endwrapup and queue_transfer are now 0 length.

--- src/cdr.erl
+++ src/cdr.erl
@@ -253,7 +253,7 @@ handle_event({mutate, Oldid, Newcallrec}, #state{id = Oldid} = State) when is_re
 			{ok, State}
 	end;
 handle_event({Transaction, #call{id = Callid} = Call, Time, Data}, #state{id = Callid, limbo_wrapup_count = Limbocount} = State) ->
-	Ended = case lists:member(Transaction, [hangup, agent_transfer, voicemail]) of
+	Ended = case lists:member(Transaction, [hangup, agent_transfer, voicemail, endwrapup, queue_transfer]) of
 		true ->
 			Time;
 		false ->

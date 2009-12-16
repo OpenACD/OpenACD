ef9b85cb6ce45ce7a62bcd94802a0c468faf6ec0
Andrew Thompson <andrew@thraddash.mgmt>
2009-12-16 16:20:55 -0500

    Enable kernel polling by default (it only warns if its not supported)

--- boot.rb
+++ boot.rb
@@ -97,5 +97,5 @@ if ! File.exists?($conf + ".config")
 end
 
 #puts "erl -pa #{$ebin} -pa contrib/mochiweb/ebin/ -setcookie #{$cookie} #{$nametype} #{$name} -config #{$conf} -boot ebin/#{$boot}"
-exec "erl -pa #{$ebin} -setcookie #{$cookie} #{$nametype} #{$name} -config #{$conf} -boot ebin/#{$boot} #{$reloader}"
+exec "erl +K true -pa #{$ebin} -setcookie #{$cookie} #{$nametype} #{$name} -config #{$conf} -boot ebin/#{$boot} #{$reloader}"
 #erl -pa ebin/ -pa contrib/mochiweb/ebin/ -setcookie ClueCon -sname testme -config single -boot ebin/cpx-rel-0.1

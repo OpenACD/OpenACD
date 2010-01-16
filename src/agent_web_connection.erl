6bea8c2ffe59b2977beeed4fc0a7635c8edd1d40
Andrew Thompson <andrew@DEV-CS1.fusedsolutions.com>
2010-01-16 16:47:06 -0500

    Prevent spurious logoffs due to "missed polls"
    The reason that agents were occasionally incorrectly logged off is
    becuase when an event came in close to the time when a poll was
    expiring, it'd send it out, clear the poll pid but not update the poll
    pid time. This meant that when the periodic timeout check ran, the last
    time it saw was when the poll pid was *established* which could well be
    longer ago than the cutoff, thus triggering a logout.

--- src/agent_web_connection.erl
+++ src/agent_web_connection.erl
@@ -964,6 +964,7 @@ handle_info(check_live_poll, #state{poll_pid_established = Last, poll_pid = unde
 	case Now - Last of
 		N when N > 10 ->
 			?NOTICE("Stopping due to missed_polls; last:  ~w now: ~w difference: ~w", [Last, Now, Now - Last]),
+			% TODO - this should probably be 'normal' or all the linked processes should handle {EXIT, missed_polls} quietly
 			{stop, missed_polls, State};
 		_N ->
 			Tref = erlang:send_after(?TICK_LENGTH, self(), check_live_poll),
@@ -1681,7 +1682,7 @@ push_event(Eventjson, State) ->
 		Pid when is_pid(Pid) ->
 			?DEBUG("Sending to the ~w", [Pid]),
 			Pid ! {poll, {200, [], mochijson2:encode({struct, [{success, true}, {<<"data">>, lists:reverse(Newqueue)}]})}},
-			State#state{poll_queue = [], poll_pid = undefined}
+			State#state{poll_queue = [], poll_pid = undefined, poll_pid_established = util:now()}
 	end.
 
 -ifdef(TEST).

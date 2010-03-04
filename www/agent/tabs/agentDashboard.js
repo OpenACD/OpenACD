91965885b5714e08af88ff218d9f4e93c03fb11c
Micah Warren <micahw@devmicah.fusedsolutions.com>
2010-03-04 15:31:09 -0500

    First pass at converting queueDashboard to just dashboard.

---
+++ www/agent/tabs/agentDashboard.js
@@ -0,0 +1,146 @@
+if(typeof(agentDashboard) == 'undefined'){
+
+	agentDashboard = function(){
+		throw 'NameSpace, not to be instanciated';
+	}
+	
+	agentDashboard.profiles = [];
+	
+	// ======
+	// Helper class Profile
+	// ======
+	
+	agentDashboard.Profile = function(display){
+		this.name = display;
+		this.agents = {};
+		this.agentsCount = 0;
+		this.avail = 0;
+		this.incall = 0;
+		this.released = 0;
+		this.wrapup = 0;
+		this._masterSubscription = dojo.subscribe('dashboard/supevent/agent', this, function(action, event){
+			this.consumeEvent(event);
+		});
+	}
+	
+	agentDashboard.Profile.prototype.consumeEvent = function(event){
+		
+	};
+	
+	// ======
+	// Helper class Agent
+	// ======
+	
+	agentDashboard.Agent = function(initEvent){
+		this.name = initEvent.details.login;
+		this.start = Math.floor(new Date().getTime() / 1000);
+		this._idleing = 0;
+		this._working = 0;
+		this._isWorking = false;
+		this._lastChangeTime = Math.floor(new Date().getTime() / 1000);
+	}
+	
+	agentDashboard.Agent.prototype.calcUtilPercent = function(){
+		var idle = this._idleing;
+		var working = this._working;
+		var now = Math.floor(new Date().getTime() / 1000);
+		if(this._isWorking){
+			working += (now - this._lastChangeTime);
+		} else {
+			idle += (now - this._lastChangeTime);
+		}
+		var total = idle + working;
+		return (idle / total) * 100;
+	}
+	
+	// =====
+	// Other Helper functions
+	// =====
+	
+	agentDashboard.drawProfileTable = function(){
+		// Call after fetching the profiles.  Should only need to be done once.
+		console.log('drawing profile table');
+		for(var i = 0; i < agentDashboard.profiles.length; i++){
+			var testnom = agentDashboard.profiles[i].name;
+			nodes = dojo.query('#agentDashboard *[profile="' + testnom + '"]');
+			if(nodes.length == 0){
+				var profCache = agentDashboard.profiles[i];
+				var profileTr = dojo.create('tr', {profile: testnom, purpose: 'profileDisplay'}, 'agentDashboardTable');
+				dojo.create('td', {purpose: 'name', innerHTML: testnom}, profileTr);
+				dojo.create('td', {purpose: 'agentCount', innerHTML: profCache.agentsCount}, profileTr);
+				dojo.create('td', {purpose: 'avail', innerHTML: profCache.avail}, profileTr);
+				dojo.create('td', {purpose: 'incall', innerHTML: profCache.incall}, profileTr);
+				dojo.create('td', {purpose: 'released', innerHTML: profCache.released}, profileTr);
+				dojo.create('td', {purpose: 'wrapup', innerHTML: profCache.wrapup}, profileTr);
+				profileTr.onclick = function(){
+					var profileNom = this.getAttribute('profile');
+					var profile = {};
+					for(var i = 0; i < agentDashboard.profiles.length; i++){
+						if(agentDashboard.profiles[i].name == profileNom){
+							profile = agentDashboard.profiles[i];
+							break;
+						}
+					}
+					var agentDisps = dojo.query('#agentDashboardTable *[profile="' + profileNom + '"][purpose="agentDisplay"]');
+					if(agentDisps.length == 0){
+						agentDashboard.drawAgentTable(profile);
+					} else {
+						dojo.byId('agentDashboardTable').removeChild(agentDisps[0]);
+					}
+				}
+				// TODO put the subscription here.
+				dojo.byId('agentDashboardTable').appendChild(profileTr);
+			}
+		}
+	}
+	
+	agentDashboard.drawAgentTable = function(profCache){
+		var profile = profCache.name;
+		console.log('drawAgentTable');
+		var profileAgentsTr = dojo.create('tr', {'profile': profile, purpose: 'agentDisplay'});
+		dojo.place(profileAgentsTr, dojo.query('#agentDashboardTable *[profile="' + profile + '"][purpose="profileDisplay"]')[0], 'after');
+		dojo.create('td', null, profileAgentsTr);
+		var widetd = dojo.create('td', {colspan: 5}, profileAgentsTr);
+		var table = dojo.create('table', null, widetd);
+		table.innerHTML = '<tr>' + 
+		'<th>name</th>' +
+		'<th>state</th>' +
+		'<th>time</th>' +
+		'<th>util</th>' +
+		'<th>Details</th>';
+		var tbody = dojo.query('#agentDashboardTable *[profile="' + profile + '"][purpose="agentDisplay"] table')[0];
+		for(var i in profCache.agents){
+			agentDashboard.drawAgentTableRow(profile, profCache[i], tbody);
+		}
+	}
+	
+	agentDashboard.drawAgentTableRow = function(profile, agent, tbody){
+		console.log('drawAgentTableRow');
+		var tr = dojo.create('tr', {'agent':agent}, tbody, 'last');
+		var now = Math.floor(new Date().getTime() / 1000);
+		dojo.create('td', {'agent':agent.name, purpose:'name', innerHTML:agent.name}, tr);
+		dojo.create('td', {'agent':agent.name, purpose:'state', innerHTML:agent.state}, tr);
+		dojo.create('td', {'agent':agent.name, purpose:'time', innerHTML:now - agent.lastchange}, tr);
+		dojo.create('td', {'agent':agent.name, purpose:'util', innerHTML:agent.util}, tr);
+		dojo.create('td', {'agent':agent.name, purpose:'details', innerHTML:agent.details}, tr);
+		//name, state, time, util, details
+	}
+}
+
+dojo.xhrGet({
+	url:'/profilelist',
+	handleAs:'json',
+	load:function(res){
+		if(res.success){
+			for(var i = 0; i < res.profiles.length; i++){
+				agentDashboard.profiles.push(res.profiles[i]);
+			}
+			agentDashboard.drawProfileTable();
+		} else {
+			errMessage(['Getting profiles failed', res.message]);
+		}
+	},
+	error:function(res){
+		errMessage(['Getting profiles errored', res]);
+	}
+});
---
+++ www/agent/tabs/queueDashboard.js
@@ -0,0 +1,435 @@
+if(typeof(queueDashboard) == "undefined"){
+		
+	queueDashboard = function(){
+		return {};
+	}
+	
+	queueDashboard.dataStore = {
+		queues:{}
+	};
+	
+	// =====
+	// helper class queue
+	// =====
+	queueDashboard.Queue = function(display){
+		this.name = display;
+		this.medias = {};
+		this._history = {};
+		this._limboMedias = {};
+		this.completed = 0;
+		this.calls = 0;
+		this.abandoned = 0;
+		this.avgHold = 0;
+		this.maxHold = 0;
+		this._masterSubscription = dojo.subscribe("queueDashboard/supevent", this, function(event){
+			this.consumeEvent(event.data);
+		});
+	}
+	
+	queueDashboard.Queue.prototype.consumeEvent = function(data){
+		debug(["nom nom'ing", data]);
+		if(this.medias[data.id]){
+			this.updateLiveMedia(data);
+		} else if(this._history[data.id]){
+			this.updateHistoricalMedia(data);
+		} else if(this._limboMedias[data.id]){
+			debug(['updating limbo', data]);
+			if(data.action == 'drop'){
+				delete this._limboMedias[data.id];
+			} else if(data.details.agent){
+				delete this._limboMedias[data.id];
+			} else if (data.details.queue && data.details.queue == this.name){
+				this.medias[data.id] = this._limboMedias[data.id];
+				this.medias[data.id].status = 'queued';
+				this.calls++;
+			} else if(data.details.queue){
+				delete this._limboMedias[data.id];
+			}
+		} else {
+			if(data.action == 'drop'){
+				return false;
+			} else if(data.details.queue == this.name){
+				this.medias[data.id] = new dashboard.Media(data);
+				this.calls++;
+			} else {
+				this._limboMedias[data.id] = new dashboard.Media(data);
+			}
+		}
+		
+		debug(['apparently data.type was not media', data.type]);
+		return false;
+	}
+	
+	queueDashboard.Queue.prototype.updateLiveMedia = function(data){
+		debug(['updating live', data]);
+		if(data.action == 'drop'){
+			this.calls--;
+			this.abandoned++;
+			this._history[data.id] = this.medias[data.id];
+			this._history[data.id].end('abandoned');
+			delete this.medias[data.id];
+		} else if(data.details.queue != this.name && ! data.details.agent){
+			// moved queue, keep track of it in history.
+			this.calls--;
+			this._history[data.id] = this.medias[data.id];
+			delete this.medias[data.id];
+		} else if(data.details.agent && ! data.details.queue){
+			this.calls--;
+			this.completed++;
+			this._history[data.id] = this.medias[data.id];
+			this._history[data.id].end('completed');
+			delete this.medias[data.id];
+		}
+	}
+	
+	queueDashboard.Queue.prototype.updateHistoricalMedia = function(data){
+		debug(['updating history', data]);
+		if(data.action == 'drop'){
+			if(this._history[data.id].status != 'completed'){
+				this.abandoned++;
+				this._history[data.id].end('abandoned');
+			}
+		} else if(data.details.agent){
+			if(this._history[data.id].status != 'completed'){
+				this.completed++;
+				this._history[data.id].end('completed');
+			}
+		}
+	}
+		
+	queueDashboard.Queue.prototype.recalc = function(){
+		var now = new Date();
+		now = Math.floor(now.getTime() / 1000);
+				
+		for(var i in this._history){
+			if(now - 3600 > this._history[i].ended){
+				if(this._history[i].status == 'abandoned'){
+					this.abandoned--;
+				} else {
+					this.completed--;
+				}
+				delete this._history[i];
+			}
+		}
+		
+		var totalAge = 0;
+		var counted = 0;
+		var oldest = 0;
+		
+		for(i in this.medias){
+			counted++;
+			var age = now - this.medias[i].created;
+			totalAge += age;
+			if(oldest < age){
+				oldest = age;
+			}
+		}
+		
+		for(i in this._history){
+			counted++;
+			var age = this._history[i].ended - this._history[i].created;
+			totalAge += age;
+			if(oldest < age){
+				oldest = age;
+			}
+		}
+		
+		this.maxHold = oldest;
+		if(counted > 0){
+			this.avgHold = Math.floor(totalAge / counted);
+		} else {
+			this.avgHold = 0;
+		}
+		
+		dojo.publish("queueDashboard/queueUpdate", [this]);
+	}
+	
+	queueDashboard.Queue.prototype.awareOfMedia = function(id){
+		if(! id.match(/^media-/)){
+			id = 'media-' + id;
+		}
+		
+		if(this.medias[id]){
+			return true;
+		}
+		
+		if(this._history[id]){
+			return true;
+		}
+		
+		if(this._limboMedias[id]){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	// =====
+	// Helpful functions
+	// =====
+	
+	queueDashboard.filterSupevent = function(supevent){
+		debug(["the subevent", supevent]);
+		if (supevent.type == 'media' || supevent.type == 'queue'){
+			return true;
+		}
+		
+		if( (supevent.action == 'drop') && supevent.id.match(/^media-/) ){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	queueDashboard.getStatus = function(){
+		dojo.xhrGet({
+			url:'/supervisor/status',
+			handleAs:'json',
+			load:function(res){
+				debug(res);
+				// nab only the medias
+				var real = [];
+				var items = res.data.items;
+				for(var i = 0; i < items.length; i++){
+					if(items[i].type == 'media'){
+						var fixedItem = {
+							action: 'set',
+							details: items[i].details._value,
+							display: items[i].display,
+							id: items[i].id,
+							type: 'media'
+						}
+						real.push(fixedItem);
+					}
+				}
+				
+				for(i = 0; i < real.length; i++){
+					debug(["status fixed", real[i]]);
+					dojo.publish("queueDashboard/supevent", [{data: real[i]}]);
+				}
+				
+				if(! queueDashboard.recalcTimer){
+					queueDashboard.recalcTimerFunc();
+				}
+				
+				queueDashboard.getHistory();
+			},
+			error:function(res){
+				errMessage(["getting initial status errored", res]);
+			}
+		});
+	}
+	
+	queueDashboard.getHistory = function(file){
+		if(! file){
+			file = "/dynamic/all.json";
+		}
+		var now = new Date();
+		now = Math.floor(now.getTime() / 1000);
+		dojo.xhrGet({
+			url:file,
+			handleAs:'json',
+			load:function(res){
+				/*for(var i = 0; i < res.queueGroups.length; i++){*/
+					/*var queues = res.queueGroups[i].queues;*/
+					for(var j = 0; j < res.queues.length; j++){
+						var queue = res.queues[j].name;
+						var medias = res.queues[j].medias;
+						var seedAbn = res.queues[j].totalAbandoned;
+						var seedComplete = res.queues[j].totalInbound - seedAbn;
+						for(var k = 0; k < medias.length; k++){
+							var media = medias[k];
+							if(media.ended && (now - media.ended > 3600)){
+								console.log(["too old", media]);
+								/*if(media.didAbandon){
+									seedAbn--;
+								} else {
+									seedComplete--;
+								}*/
+								continue;
+							}
+							
+							if(queueDashboard.dataStore.queues[queue].awareOfMedia(media.id)){
+								console.log(["already knew", media]);
+								/*if(media.didAbandon){
+									seedAbn--;
+								} else {
+									seedComplete--;
+								}*/
+								continue;
+							}
+							
+							media.id = 'media-' + media.id;
+							var initEvent = {
+								details:{
+									queued_at: {
+										timestamp: media.queued
+									},
+									type: media.type,
+									client: media.brand
+								}
+							};
+							var usableMedia = new dashboard.Media(initEvent);
+							
+							usableMedia.note = 'transformed from historical data';
+							if(media.ended){
+								if(media.didAbandon){
+									usableMedia.status = 'abandoned';
+									usableMedia.ended = media.ended;
+									queueDashboard.dataStore.queues[queue].abandoned++;
+									console.log(['abandoned', media]);
+								} else {
+									usableMedia.status = 'completed';
+									usableMedia.ended = media.ended;
+									queueDashboard.dataStore.queues[queue].completed++;
+									console.log(['completed', media]);
+								}
+								queueDashboard.dataStore.queues[queue]._history[media.id] = usableMedia;
+							} else {
+								usableMedia.status = 'queued';
+								queueDashboard.dataStore.queues[queue].medias[media.id] = usableMedia;
+								queueDashboard.dataStore.queues[queue].calls++;
+							}
+						}
+					}
+				/*}*/
+			},
+			error:function(res){
+				var m = ["getting historical data errored", res];
+				warning(m);
+				errMessage(m);
+			}
+		});
+	}
+	
+	queueDashboard.drawQueueTable = function(){
+		// Should only need to be called once, after the queue list is gotten.
+		for(var i in queueDashboard.dataStore.queues){
+			var testnom = i;
+			nodes = dojo.query('#queueDashboardTable *[queue="' + testnom + '"]');
+			if(nodes.length == 0){
+				var queueTr = document.createElement('tr');
+				queueTr.setAttribute('queue', testnom);
+				queueTr.setAttribute('purpose', 'queueDisplay');
+				dojo.create('td', {purpose: 'name', innerHTML: testnom}, queueTr);
+				dojo.create('td', {purpose: 'callCount'}, queueTr);
+				dojo.create('td', {purpose: 'completeCount'}, queueTr);
+				dojo.create('td', {purpose: 'abandonCount'}, queueTr);
+				dojo.create('td', {purpose: 'averageHold'}, queueTr);
+				dojo.create('td', {purpose: 'oldestHold'}, queueTr);
+				queueTr.onclick = function(){
+					var queuenom = this.getAttribute('queue');
+					var callDisps = dojo.query('#queueDashboardTable *[queue="' + queuenom + '"][purpose="callDisplay"]');
+					if(callDisps.length == 0){
+						queueDashboard.drawCallTable(queuenom);
+					} else {
+						dojo.byId('queueDashboardTable').removeChild(callDisps[0]);
+					}
+				}
+				queueTr.queueSubscription = dojo.subscribe("queueDashboard/queueUpdate", queueTr, function(queue){
+					if(queue.name != this.getAttribute('queue')){
+						return false;
+					}
+					var queuenom = this.getAttribute('queue');
+					var tds = dojo.query('#queueDashboardTable *[queue="' + queuenom + '"][purpose="queueDisplay"] td');
+					for(var j = 0; j < tds.length; j++){
+						switch(tds[j].getAttribute('purpose')){
+							case 'callCount':
+								tds[j].innerHTML = queue.calls;
+								break;
+							case 'completeCount':
+								tds[j].innerHTML = queue.completed;
+								break;
+							case 'abandonCount':
+								tds[j].innerHTML = queue.abandoned;
+								break;
+							case 'averageHold':
+								tds[j].innerHTML = formatseconds(queue.avgHold);
+								break;
+							case 'oldestHold':
+								tds[j].innerHTML = formatseconds(queue.maxHold);
+								break;
+							default:
+								// do nothing, nothing!
+						}
+					}
+				});
+				dojo.byId('queueDashboardTable').appendChild(queueTr);
+			}
+		}
+	}
+	
+	queueDashboard.drawCallTable = function(queuename){
+		var queueMediasTr = document.createElement('tr');
+		queueMediasTr.setAttribute('queue', queuename);
+		queueMediasTr.setAttribute('purpose', 'callDisplay');
+		dojo.place(queueMediasTr, dojo.query('#queueDashboardTable *[queue="' + queuename + '"][purpose="queueDisplay"]')[0], 'after');
+		dojo.create('td', null, queueMediasTr);
+		var widetd = dojo.create('td', {colspan: 5}, queueMediasTr);
+		var table = dojo.create('table', null, widetd);
+		table.innerHTML = '<tr>' +
+		'<th>Caller ID</th>' +
+		'<th>Type</th>' +
+		'<th>Hold Time</th>' +
+		'<th>Brand</th>' +
+		'</tr>';
+		var tbody = dojo.query('#queueDashboardTable *[queue="' + queuename + '"][purpose="callDisplay"] table')[0];
+		
+		for(var i in queueDashboard.dataStore.queues[queuename].medias){
+			queueDashboard.drawCallTableRow(queuename, i, tbody);
+		}
+	}
+	
+	queueDashboard.drawCallTableRow = function(queuename, mediaid, tbody){
+		var tr = document.createElement('tr');
+		var media = queueDashboard.dataStore.queues[queuename].medias[mediaid];
+		var now = Math.floor(new Date().getTime() / 1000);
+		var age = now - media.created;
+		tr.setAttribute('callid', mediaid);
+		dojo.create('td', {innerHTML: media.initalEvent.details.callid_name + " " + media.initalEvent.details.callid_data}, tr);
+		dojo.create('td', {innerHTML: media.type}, tr);
+		dojo.create('td', {innerHTML: formatseconds(age)}, tr);
+		dojo.create('td', {innerHTML: media.client}, tr);
+		dojo.place(tr, tbody, 'last');
+	}
+	
+	queueDashboard.recalcTimerFunc = function(){
+		if(dojo.byId('queueDashboardTable')){
+			for(var i in queueDashboard.dataStore.queues){
+				queueDashboard.dataStore.queues[i].recalc();
+			}
+			queueDashboard.recalcTimer = setTimeout(queueDashboard.recalcTimerFunc, 5000);
+		}
+	}
+}
+
+dojo.xhrGet({
+	url:'/queuelist',
+	handleAs:'json',
+	load:function(res){
+		if(res.success){
+			for(var i = 0; i < res.queues.length; i++){
+				queueDashboard.dataStore.queues[res.queues[i].name] = new queueDashboard.Queue(res.queues[i].name);
+			}
+			queueDashboard.drawQueueTable();
+			queueDashboard.getStatus();
+		} else {
+			errMessage(["getting queues failed", res.message]);
+		}
+	},
+	error:function(res){
+		errMessage(["getting queues errored", res]);
+	}
+});
+
+queueDashboard.mediaSub = dojo.subscribe('dashboard/supevent/media', queueDashboard, function(action, supevent){
+	if(! this.filterSupevent(supevent)){
+		return false;
+	}
+	var supcp = supevent;
+	//supcp.data.id = supcp.data.id.substr(6);
+
+	debug(["queueDashboard forwarding", supcp]);
+
+	dojo.publish("queueDashboard/supevent", [supcp]);
+});

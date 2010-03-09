f3b857baf589ce4d2db0b3724b60987b51a84ae6
Andrew Thompson <andrew@hijacked.us>
2010-03-09 16:09:47 -0500

    Hack on the stupid supervisor dashboard

--- www/agent/tabs/queueDashboard.js
+++ www/agent/tabs/queueDashboard.js
@@ -25,6 +25,7 @@ if(typeof(queueDashboard) == "undefined"){
 	
 	queueDashboard.Queue.prototype.consumeEvent = function(data){
 		//console.log(["nom nom'ing", data]);
+		/*console.log(data);*/
 		if(data.type != 'media'){
 			return false;
 		}
@@ -39,6 +40,7 @@ if(typeof(queueDashboard) == "undefined"){
 			return true;
 		}
 		
+		/* wtf? */
 		if(data.details.queue != this.name && this.medias[data.name]){
 			delete this.medias[data.name];
 			var nodes = dojo.query('#queueDashboardTable tr[callid="' + data.name + '"]');
@@ -47,16 +49,20 @@ if(typeof(queueDashboard) == "undefined"){
 				n.parentNode.removeChild(n);
 			}			
 			dojo.publish('queueDashboard/updateQueue/' + data.name, [{action:'drop'}]);
-		}
-		
-		if(data.details.queue == this.name){
+		} else if(data.details.queue == this.name){
 			this.medias[data.name] = data;
 			dojo.publish('queueDashboard/updateQueue/' + data.name, [{action:'set'}]);
-			var nodes = dojo.query('#queueDashboardTable tr[callid="' + data.name = '"]');
+			var nodes = dojo.query('#queueDashboardTable tr[callid="' + data.name + '"]');
 			if(nodes.length == 0){
-			
+				/* add row */
+				var rows = dojo.query('#queueDashboardTable *[queue="' + data.details.queue + '"][purpose="callDisplay"] table');
+				if(rows.length == 1) {
+					var tbody = dojo.query('#queueDashboardTable *[queue="' + data.details.queue + '"][purpose="callDisplay"] table')[0];
+					queueDashboard.drawCallTableRow(data.details.queue, data.name, tbody);
+				}
 			} else {
-			
+				/* update row */
+				console.log("update row NYI");
 			}
 		}
 		
@@ -300,6 +306,7 @@ if(typeof(queueDashboard) == "undefined"){
 			if(nodes.length == 0){
 				var queueTr = document.createElement('tr');
 				queueTr.setAttribute('queue', testnom);
+				queueTr.setAttribute('class', "queueDisplay");
 				queueTr.setAttribute('purpose', 'queueDisplay');
 				queueTr.setAttribute('expanded', false);
 				dojo.create('td', {purpose: 'name', innerHTML: testnom}, queueTr);
@@ -322,14 +329,17 @@ if(typeof(queueDashboard) == "undefined"){
 					if(ex){
 						console.log('should try to collapse');
 						var callDisps = dojo.query('#queueDashboardTable *[queue="' + queuenom + '"][purpose="callDisplay"]');
-						dojo.byId('queueDashboardTable').removeChild(callDisps[0]);
+						callDisps[0].style.display = "none";
 					} else {
 						console.log('should try to expand');
-						queueDashboard.drawCallTable(queuenom);
+						var callDisps = dojo.query('#queueDashboardTable *[queue="' + queuenom + '"][purpose="callDisplay"]');
+						callDisps[0].style.display = "";
+						/*queueDashboard.drawCallTable(queuenom);*/
 					}
 					console.log('toggling state');
 					this.setAttribute('expanded', ! ex);
 				}
+
 				queueTr.queueSubscription = dojo.subscribe("queueDashboard/queueUpdate", queueTr, function(queue){
 					if(queue.name != this.getAttribute('queue')){
 						return false;
@@ -359,6 +369,8 @@ if(typeof(queueDashboard) == "undefined"){
 					}
 				});
 				dojo.byId('queueDashboardTable').appendChild(queueTr);
+
+				queueDashboard.drawCallTable(testnom);
 			}
 		}
 	}
@@ -367,35 +379,137 @@ if(typeof(queueDashboard) == "undefined"){
 		var queueMediasTr = document.createElement('tr');
 		queueMediasTr.setAttribute('queue', queuename);
 		queueMediasTr.setAttribute('purpose', 'callDisplay');
+		queueMediasTr.setAttribute('style', 'display:none;');
 		dojo.place(queueMediasTr, dojo.query('#queueDashboardTable *[queue="' + queuename + '"][purpose="queueDisplay"]')[0], 'after');
-		dojo.create('td', null, queueMediasTr);
-		var widetd = dojo.create('td', {colspan: 5}, queueMediasTr);
-		var table = dojo.create('table', null, widetd);
+		/*dojo.create('td', null, queueMediasTr);*/
+		var widetd = dojo.create('td', {colspan: 6}, queueMediasTr);
+		var table = dojo.create('table', {width: "100%"}, widetd);
 		table.innerHTML = '<tr>' +
 		'<th>Caller ID</th>' +
 		'<th>Type</th>' +
-		'<th>Hold Time</th>' +
+		'<th>Time</th>' +
 		'<th>Brand</th>' +
 		'</tr>';
-		var tbody = dojo.query('#queueDashboardTable *[queue="' + queuename + '"][purpose="callDisplay"] table')[0];
+
+		/* hack to make alternate rows color right */
+		var tr = document.createElement('tr');
+		tr.setAttribute('style', 'display:none;');
+		dojo.place(tr, dojo.query('#queueDashboardTable *[queue="' + queuename + '"][purpose="queueDisplay"]')[0], 'after');
+		/*var tbody = dojo.query('#queueDashboardTable *[queue="' + queuename + '"][purpose="callDisplay"] table')[0];*/
 		
-		for(var i in queueDashboard.dataStore.queues[queuename].medias){
-			queueDashboard.drawCallTableRow(queuename, i, tbody);
-		}
+		/*for(var i in queueDashboard.dataStore.queues[queuename].medias){*/
+			/*queueDashboard.drawCallTableRow(queuename, i, tbody);*/
+		/*}*/
 	}
 	
 	queueDashboard.drawCallTableRow = function(queuename, mediaid, tbody){
 		var tr = document.createElement('tr');
 		var media = queueDashboard.dataStore.queues[queuename].medias[mediaid];
+		if(!media) {
+			console.log("Can't find "+mediaid);
+			return; /* TODO - why does this occur? */
+		}
 		var now = Math.floor(new Date().getTime() / 1000);
 		var age = now - media.details.queued_at.timestamp;
 		tr.setAttribute('callid', mediaid);
 		dojo.create('td', {purpose:'callerid', innerHTML: media.details.callid_name + " " + media.details.callid_data}, tr);
 		dojo.create('td', {purpose: 'mediaType', innerHTML: '<img src="/images/' + media.details.type + '.png" />'}, tr);
 		dojo.create('td', {purpose: 'age', innerHTML: formatseconds(age)}, tr);
-		dojo.create('td', {purpose: 'client', innerHTML: media.client}, tr);
+		dojo.create('td', {purpose: 'client', innerHTML: media.details.client}, tr);
 		dojo.place(tr, tbody, 'last');
+		tr.onclick = function(event){
+			var menu = new dijit.Menu({});
+			menu.addChild(new dijit.MenuItem({
+				label: "Send To Agent...",
+				onClick: function() { queueDashboard.sendToAgentDialog(mediaid, queuename);}
+			}));
+
+			if (media.details.type == "email") {
+				menu.addChild(new dijit.MenuItem({
+					label: "Peek",
+					onClick: function() {alert("peek");}
+				}));
+			} else if (media.details.type == "voice") {
+				menu.addChild(new dijit.MenuItem({
+					label: "Send to voicemail",
+					onClick: function() {alert("voicemail");}
+				}));
+			}
+			menu._openMyself(event);
+		}
 	}
+
+	queueDashboard.sendToAgentDialog = function(mediaID, queue){
+		console.log(["mediaID", mediaID]);
+		dojo.xhrGet({
+			url:'/get_avail_agents',
+			handleAs:'json',
+			load: function(res){
+				if(res.success){
+					console.log(res.agents);
+					var selectContent = '';
+					if(res.agents.length == 0){
+						errMessage('No agents available!');
+						return false;
+					}
+					for(var i = 0; i < res.agents.length; i++){
+						selectContent += '<option value="' + res.agents[i].name + '">' + res.agents[i].name + ' (' + res.agents[i].profile + ')</option>';
+					}
+					var content = '<p><label>Agent:</label><select name="agent" id="supSelectAgent">' + selectContent + '</select></p><p><label>&nbsp;</label><input type="submit" dojoType="dijit.form.Button" label="Submit" /></p>';
+					var dialog = new dijit.Dialog({
+						title:'Select Agent',
+						content: content
+					});
+					dialog.attr('execute', function(){
+						var agentName = dojo.byId('supSelectAgent').value;
+						dialog.destroy();
+						console.log([agentName, arguments]);
+
+						var simpleObj = {
+							id: mediaID,
+							queue: queue
+						};
+
+						queueDashboard.sendMediaToAgent(simpleObj, agentName);
+					});
+					dialog.show();
+					return true;
+				}
+				errMessage(['getting available agents failed', res.message]);
+			},
+			error: function(res){
+				errMessage(['getting available agents errored', res]);
+			}
+		});
+	};
+
+	queueDashboard.sendMediaToAgent = function(media, agent){
+		if(! agent){
+			return false;
+		}
+		
+		if(media.queue){
+			var queue = media.queue;
+			var id = media.id;
+			return dojo.xhrGet({
+				handleAs:"json",
+				url:"/supervisor/agent_ring/" + escape(queue) + "/" + escape(id) + "/" + escape(agent),
+				load:function(res){
+					if(res.success){
+						//kewl
+						return true;
+					}
+					else{
+						errMessage(["agent ring failed", res.message]);
+					}
+				},
+				error:function(res){
+					errMessage(["agent ring errored", res]);
+				}
+			});
+		}
+	};
+
 	/*
 	queueDashboard.recalcTimerFunc = function(){
 		if(dojo.byId('queueDashboardTable')){

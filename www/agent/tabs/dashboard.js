553572c96c932505205bf9faaf6d2a1616f91bd8
Micah Warren <micahw@devmicah.fusedsolutions.com>
2010-03-11 11:33:26 -0500

    Dynamically created 'Dashboard' menu.

--- www/agent/tabs/dashboard.js
+++ www/agent/tabs/dashboard.js
@@ -82,6 +82,121 @@ if(typeof(dashboard) == 'undefined'){
 			}
 		});	
 	}
+	
+	// =====
+	// Menu Item actions
+	// =====
+	
+	dashboard.showMotdDialog = function(nodename){
+		dojo.xhrGet({
+			url:'/supervisor/getmotd',
+			handleAs:'json',
+			load:function(res){
+				if(! res.success){
+					errMessage(["Failed getting motd", res.message]);
+					return false;
+				}
+				
+				var dialog = dijit.byId("blabDialog");
+				dialog.attr('title', 'MotD');
+				if(res.motd){
+					dialog.attr('value', {'message':res.motd});
+				} else {
+					dialog.attr('value', {'message':'Type the Message of the Day here.  Leave blank to unset.'});
+				}
+				var submitblab = function(){
+					var data = dialog.attr('value').message;
+					dojo.xhrPost({
+						url:'/supervisor/motd',
+						handleAs:'json',
+						content:{
+							message:data,
+							node:nodename
+						},
+						load:function(res){
+							if(res.success){
+								return true;
+							}
+							errMessage(["setting motd failed", res.message]);
+						},
+						error:function(res){
+							errMessage(["setting motd errored", res]);
+						}
+					});
+				}
+				dialog.attr('execute', submitblab);
+				dialog.show();
+			},
+			error: function(res){
+				errMessage(["Errored getting motd", res]);
+			}
+		});
+	}
+	
+	dashboard.showProblemRecordingDialog = function(){
+		dojo.xhrGet({
+			url:'/brandlist',
+			handleAs:'json',
+			load:function(res){
+				if(! res.success){
+					errMessage(["failed loading brands", res.message]);
+					return false;
+				}
+
+				var sel = dojo.byId('supervisorClientSelect');
+				for(var i = 0; i < res.brands.length; i++){
+					var optionnode = document.createElement('option');
+					optionnode.value = res.brands[i].id;
+					optionnode.innerHTML = res.brands[i].label;
+					sel.appendChild(optionnode);
+				}
+
+				var dialog = dijit.byId('setProblemRecording');
+				dialog.attr('execute', function(){
+					var clientId = dojo.byId('supervisorClientSelect').value;
+					if(dialog.attr('value').set.length < 1){
+						dojo.xhrGet({
+							url:'/supervisor/remove_problem_recording/' + escape(clientId),
+							handleAs: 'json',
+							load:function(res){
+								if(res.success){
+									return true
+								}
+
+								errMessage(['removing problem recording failed', res.message]);
+							},
+							error:function(res){
+								errMessage(['error removing problem recording', res]);
+							}
+						});
+					} else {
+						dashboard.startProblemRecording(clientId);
+					}
+				});
+				dialog.show();
+			},
+			error: function(res){
+				errMessage(["error loading brands", res]);
+			}
+		});	
+	}
+	
+	dashboard.startProblemRecording = function(clientId){
+		dojo.xhrGet({
+			url: '/supervisor/start_problem_recording/' + agent.login + '/' + escape(clientid),
+			handleAs: 'json',
+			load: function(res){
+				if(res.success){
+					return true;
+				}
+				
+				errMessage(['Starting problem recording failed', res.message]);
+			},
+			error: function(res){
+				errMessage(['Starting problem recofing errored', res]);
+			}
+		});
+	}
 }
 
 dashboard.masterSub = dojo.subscribe("agent/supervisortab", dashboard, function(supevent){
@@ -97,3 +212,46 @@ dashboard.masterSub = dojo.subscribe("agent/supervisortab", dashboard, function(
 	
 	dojo.publish('dashboard/supevent/' + supevent.data.type, [supevent.data]);
 });
+
+dashboard.unloadSub = dojo.subscribe('tabPanel-removeChild', function(child){
+	if(child.title == 'Dashboard'){
+		dojo.unsubscribe(dashboard.unloadSub);
+		var menu = dijit.byId('dashboardMenu');
+		if(menu){
+			menu.destroy();
+		}
+	}
+});
+
+var menu = dijit.byId('dashboardMenu');
+if(menu){
+	menu.destroy();
+}
+
+var menu = new dijit.Menu({
+	style:'display:none'
+});
+
+menu.addChild(new dijit.MenuItem({
+	label:'Set Motd...',
+	onClick:function(){
+		dashboard.showMotdDialog('system');
+		//errMessage('motd nyi');
+	}
+}));
+
+menu.addChild(new dijit.MenuItem({
+	label:'Record Problem...',
+	onClick:function(){
+		dashboard.showProblemRecordingDialog();
+	}
+}));
+
+var button = new dijit.form.DropDownButton({
+	label: 'Dashboard',
+	name: 'dashboardMenu',
+	dropDown: menu,
+	id: 'dashboardMenu'
+});
+
+dojo.byId("menubar").appendChild(button.domNode);

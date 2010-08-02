ca26de821b260e2dc2c2107a72b1b0ac9e6a5c9d
Andrew Thompson <andrew@hijacked.us>
2010-08-02 08:33:17 -0400

    Optimize message formatting for log messages written to 2+ log files
    Instead of calling io_lib:format multiple times, we call it once per
    message (assuming any file backends are setup to handle the message).
    This can make file logging up to N times faster, where N is the number
    of logfiles configured (default is 2).

--- src/cpxlog_file.erl
+++ src/cpxlog_file.erl
@@ -86,21 +86,22 @@ handle_event({Level, Time, Module, Line, Pid, Message, Args}, State) ->
 		false ->
 			ok
 	end,
-	lists:foreach(fun({_, FH, _, LogLevel}) ->
-				case ((lists:member(Level, ?LOGLEVELS) andalso (util:list_index(Level, ?LOGLEVELS) >= util:list_index(LogLevel, ?LOGLEVELS)))) of
-					true ->
-						file:write(FH,
-							io_lib:format("~w:~s:~s [~s] ~w@~s:~w ~s~n", [
-									element(1, element(2, Time)),
-									string:right(integer_to_list(element(2, element(2, Time))), 2, $0),
-									string:right(integer_to_list(element(3, element(2, Time))), 2, $0),
-									string:to_upper(atom_to_list(Level)),
-									Pid, Module, Line,
-									io_lib:format(Message, Args)]));
-					false ->
-						ok
-				end
+	Files = lists:filter(fun({_, FH, _, LogLevel}) ->
+				lists:member(Level, ?LOGLEVELS) andalso (util:list_index(Level, ?LOGLEVELS) >= util:list_index(LogLevel, ?LOGLEVELS))
 		end, Filehandles),
+	case Files of
+		[] ->
+			ok;
+		List ->
+			Output = io_lib:format("~w:~s:~s [~s] ~w@~s:~w ~s~n", [
+					element(1, element(2, Time)),
+					string:right(integer_to_list(element(2, element(2, Time))), 2, $0),
+					string:right(integer_to_list(element(3, element(2, Time))), 2, $0),
+					string:to_upper(atom_to_list(Level)),
+					Pid, Module, Line,
+					io_lib:format(Message, Args)]),
+			[file:write(FH, Output) || {_, FH, _, _} <- List]
+	end,
 	{ok, State#state{lasttime = Time, filehandles = Filehandles}};
 handle_event({Level, Time, Pid, Message, Args}, State) ->
 	Filehandles = check_filehandles(State#state.filehandles),
@@ -112,21 +113,22 @@ handle_event({Level, Time, Pid, Message, Args}, State) ->
 		false ->
 			ok
 	end,
-	lists:foreach(fun({_, FH, _, LogLevel}) ->
-				case ((lists:member(Level, ?LOGLEVELS) andalso (util:list_index(Level, ?LOGLEVELS) >= util:list_index(LogLevel, ?LOGLEVELS)))) of
-					true ->
-						file:write(FH,
-							io_lib:format("~w:~s:~s [~s] ~w ~s~n", [
-									element(1, element(2, Time)),
-									string:right(integer_to_list(element(2, element(2, Time))), 2, $0),
-									string:right(integer_to_list(element(3, element(2, Time))), 2, $0),
-									string:to_upper(atom_to_list(Level)),
-									Pid,
-									io_lib:format(Message, Args)]));
-					false ->
-						ok
-				end
+	Files = lists:filter(fun({_, FH, _, LogLevel}) ->
+				lists:member(Level, ?LOGLEVELS) andalso (util:list_index(Level, ?LOGLEVELS) >= util:list_index(LogLevel, ?LOGLEVELS))
 		end, Filehandles),
+	case Files of
+		[] ->
+			ok;
+		List ->
+			Output = io_lib:format("~w:~s:~s [~s] ~w ~s~n", [
+					element(1, element(2, Time)),
+					string:right(integer_to_list(element(2, element(2, Time))), 2, $0),
+					string:right(integer_to_list(element(3, element(2, Time))), 2, $0),
+					string:to_upper(atom_to_list(Level)),
+					Pid,
+					io_lib:format(Message, Args)]),
+			[file:write(FH, Output) || {_, FH, _, _} <- List]
+	end,
 	{ok, State#state{lasttime = Time, filehandles = Filehandles}};
 handle_event(_Event, State) ->
 	{ok, State}.

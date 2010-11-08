8017f4b68376cbbe69b05e395067aaf592dd1891
Micah Warren <Micah.Warren@kgb.com>
2010-11-08 17:48:33 -0600

    Updated to conform to new cpx_mon

--- src/gen_media.erl
+++ src/gen_media.erl
@@ -1407,7 +1407,7 @@ set_cpx_mon(#state{callrec = Call} = _State, Details) ->
 			{[{inqueue, {0, 60 * 5, 60 * 10, {time, util:now()}}}], [{queued_at, {timestamp, util:now()}}, {priority, Call#call.priority} | MidBasedet]}
 	end,
 	Fulldet = lists:append([Basedet, Details]),
-	cpx_monitor:set({media, Call#call.id}, Hp, Fulldet).
+	cpx_monitor:set({media, Call#call.id}, Fulldet, self()).
 
 priv_queue(Queue, Callrec, Failover) ->
 	case queue_manager:get_queue(Queue) of

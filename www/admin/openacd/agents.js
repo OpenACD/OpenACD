617abefc2d9eeee60ab31d21c93b8296674147e7
Micah Warren <Micah.Warren@kgb.com>
2010-11-09 09:20:09 -0600

    Simplified some code in the agent modules mgt ui.

--- www/admin/openacd/agents.js
+++ www/admin/openacd/agents.js
@@ -29,15 +29,7 @@ agents.getModules = function(targetform){
 			targetform.set('value', response.result);
 			var kids = targetform.getDescendants();
 			for(var i = 0; i < kids.length; i++){
-				if(kids[i].id == 'agentModuleTCPListenEnabled'){
-					kids[i].set('checked', response.result.agentModuleTCPListenEnabled);
-				}
-				if(kids[i].id == 'agentModuleWebListenEnabled'){
-					kids[i].set('checked', response.result.agentModuleWebListenEnabled);
-				}
-				if(kids[i].id = 'agentModuleDialplanListenEnabled'){
-					kids[i].set('checked', response.result.agentModuleDialplanListenEnabled);
-				}
+				kids[i].set('checked', response.result[kids[i].id]);
 			}
 		}
 	});

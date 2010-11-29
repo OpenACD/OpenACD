4d806968249fb223db163adf5d684a7922c82fb8
Micah Warren <Micah.Warren@kgb.com>
2010-11-29 16:54:27 -0600

    A few more tests.

--- src/cpx_monitor_kgb_eventlog.erl
+++ src/cpx_monitor_kgb_eventlog.erl
@@ -464,7 +464,38 @@ file_handling_test_() ->
 		timer:sleep(5),
 		?assertMatch({ok, Bin}, file:read_file(?test_file))
 	end} end]}.
-		
+
+-record(write_test_rec, {
+	logpid,
+	ets,
+	localhost,
+	node
+}).
+
+send_agent_set(Pid, drop) ->
+	Pid ! {cpx_monitor_event, {drop, erlang:now(), {agent, "agent"}}};
+send_agent_set(Pid, State) when is_atom(State) ->
+	send_agent_set(Pid, [{state, State}]);
+send_agent_set(Pid, Props) ->
+	FullProps = [{login, "agentName"}, {skills, [{'_queue', "Queue"}]}, {node, node()} | Props],
+	Pid ! {cpx_monitor_event, {set, erlang:now(), {{agent, "agent"}, FullProps, node()}}}.
+
+send_media_set(Pid) ->
+	send_media_set(Pid, []).
+
+send_media_set(Pid, drop) ->
+	Pid ! {cpx_monitor_event, {drop, erlang:now(), {media, "media"}}};
+send_media_set(Pid, InProps) ->
+	Props = [
+		{callerid, "2318232979*12358497*112*9080"},
+		{dnis, "9080"},
+		{node, node()}
+	],
+	Pid ! {cpx_monitor_event, {set, erlang:now(), {{media, "media"}, Props, node()}}}.
+
+%% Yes, these use ?assert(true).  The real test are the pattern matching.
+%% because I want to use Rest, I'm doing the match directly rather than 2x.
+%% someone more ambitious than I can change it.
 formatting_test_() ->
 	{foreach,
 	fun() ->
@@ -475,33 +506,86 @@ formatting_test_() ->
 			{ok, [], State}
 		end),
 		{ok, EventLog} = start([{filename, ?test_file}]),
-		{EventLog, Ets}
+		Localhost = list_to_binary(net_adm:localhost()),
+		LocalhostSize = size(Localhost),
+		Node = list_to_binary(atom_to_list(node())),
+		NodeSize = size(Node),
+		#write_test_rec{
+			logpid = EventLog,
+			ets = Ets,
+			localhost = {Localhost, LocalhostSize},
+			node = {Node, NodeSize}
+		}
 	end,
-	fun({EventLog, Ets}) ->
+	fun(#write_test_rec{logpid = EventLog, ets = Ets} = _) ->
 		exit(EventLog, kill),
 		gen_leader_mock:stop(whereis(queue_manager)),
 		%file:delete(?test_file),
 		ets:delete(Ets),
 		timer:sleep(10) % letting everything die...
 	end,
-	[fun({EventLog, _}) -> {"An agent logs in", fun() ->
-		EventLog ! {cpx_monitor_event, {set, erlang:now(), {{agent, "agent"}, [{login, "agentName"}, {skills, [{'_queue', "Queue"}]}, {node, node()}], node()}}},
+	[fun(#write_test_rec{logpid = EventLog, localhost = {Lhost, LhSize}, node = {Node, NodeSize}} = _) -> {"An agent logs in", fun() ->
+		send_agent_set(EventLog, released),
 		timer:sleep(5),
 		{ok, Bin} = file:read_file(?test_file),
 		?DEBUG("Got bin:  ~p", [Bin]),
-		Localhost = list_to_binary(net_adm:localhost()),
-		LhostSize = size(Localhost),
-		Node = list_to_binary(atom_to_list(node())),
-		NodeSize = size(Node),
-		?DEBUG("Localhost:  ~s~nNode:  ~s", [Localhost, Node]),
-		<<_:17/binary, " : ", Localhost:LhostSize/binary, " : ", 
+		<<_:17/binary, " : ", Lhost:LhSize/binary, " : ", 
 		_:27/binary, " : agent_start : ", Node:NodeSize/binary, 
 		" : agentName", _:1/binary, Rest/binary>> = Bin,
 		?DEBUG("Rest:  ~p", [Rest]),
-		<<_:17/binary, " : ", Localhost:LhostSize/binary, " : ",
+		<<_:17/binary, " : ", Lhost:LhSize/binary, " : ",
 		_:27/binary, " : agent_login : ", Node:NodeSize/binary,
 		" : agentName : Queue", _:1/binary>> = Rest,
 		?assert(true)
-	end} end]}.
+	end} end,
+	fun(#write_test_rec{logpid = EventLog, localhost = {Lhost, LhSize}, node = {Node, NodeSize}} = _) -> {"An agent goes available", fun() ->
+		send_agent_set(EventLog, released),
+		timer:sleep(5),
+		file:delete(?test_file),
+		send_agent_set(EventLog, idle),
+		timer:sleep(5),
+		{ok, Bin} = file:read_file(?test_file),
+		?DEBUG("Got bin:  ~p", [Bin]),
+		<<_:17/binary, " : ", Lhost:LhSize/binary, " : ",
+		_:27/binary, " : agent_available : ", Node:NodeSize/binary,
+		" : agentName : Queue", _:1/binary, Rest/binary>> = Bin,
+		<<>> = Rest,
+		?assert(true)
+	end} end,
+	fun(#write_test_rec{logpid = EventLog, localhost = {Lhost, LhSize}, node = {Node, NodeSize}} = _) -> {"An agent goes unavailable", fun() ->
+		send_agent_set(EventLog, released),
+		send_agent_set(EventLog, idle),
+		timer:sleep(5),
+		file:delete(?test_file),
+		send_agent_set(EventLog, released),
+		timer:sleep(5),
+		{ok, Bin} = file:read_file(?test_file),
+		?DEBUG("Got bin:  ~p", [Bin]),
+		<<_:17/binary, " : ", Lhost:LhSize/binary, " : ",
+		_:27/binary, " : agent_unavailable : ", Node:NodeSize/binary,
+		" : agentName : Queue", _:1/binary, Rest/binary>> = Bin,
+		<<>> = Rest,
+		?assert(true)
+	end} end,
+	fun(#write_test_rec{logpid = EventLog, localhost = {Lhost, LhSize}, node = {Node, NodeSize}} = _) -> {"An agent logs out", fun() ->
+		send_agent_set(EventLog, released),
+		send_agent_set(EventLog, idle),
+		timer:sleep(5),
+		file:delete(?test_file),
+		send_agent_set(EventLog, drop),
+		timer:sleep(5),
+		{ok, Bin} = file:read_file(?test_file),
+		?DEBUG("Got bin:  ~p", [Bin]),
+		<<_:17/binary, " : ", Lhost:LhSize/binary, " : ",
+		_:27/binary, " : agent_stop : ", Node:NodeSize/binary,
+		" : agentName", _:1/binary, Rest/binary>> = Bin,
+		?DEBUG("Got rest:  ~p", [Rest]),
+		<<_:17/binary, " : ", Lhost:LhSize/binary, " : ",
+		_:27/binary, " : agent_logout : ", Node:NodeSize/binary,
+		" : agentName : Queue", _:1/binary>> = Rest,
+		?assert(true)
+	end} end
+	%fun(#write_test_rec{logpid = EventLog, localhost = {Lhost, LhSize}, node = {Node, NodeSize}} = _) -> {"a call enters queue", fun() ->
+		]}.
 		
 -endif.

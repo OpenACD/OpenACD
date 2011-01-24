d14786d613ec897314c227b66fed921384df9f43
Micah Warren <Micah.Warren@kgb.com>
2011-01-24 16:52:38 -0600

    Added web config for plugin_dir.

--- priv/www/admin/lang/nls/cpx_supervisor.js
+++ priv/www/admin/lang/nls/cpx_supervisor.js
@@ -7,5 +7,6 @@ TRANSFER_SKILLS: "Skills",
 DEFAULT_RINGOUT_EXPLANATION:'This is in seconds.',
 DEFAULT_RINGOUT:'Ringout',
 MAX_RINGOUTS_EXPLANATION: "Agents missing this many rings will be logged out.  Media that fails to ring the agent do not count toward this.  The number is reset only when the agent goes oncall.",
-MAX_RINGOUTS: "Max Ringouts"
+MAX_RINGOUTS: "Max Ringouts",
+PLUGIN_DIR: "Plugin Directory"
 }

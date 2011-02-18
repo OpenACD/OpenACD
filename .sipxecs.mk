1aaed88292c6d3724c46cca1e22b020355d209f3
Douglas Hubler <dhubler@ezuce.com>
2011-02-18 15:03:52 -0500

    minor update to sipxecs-openacd build integration

--- .sipxecs.mk
+++ .sipxecs.mk
@@ -1,7 +1,7 @@
 # See ./src/OpenACD.app.src for matching version number. Please keep in sync, but not strictly nec.
 OpenACD_VER = 0.9.5
 
-OpenACD_REV := $(shell cd $(SRC)/OpenACD; $(SRC)/config/revision-gen $(OpenACD_VER))
+OpenACD_REV = $(shell cd $(SRC)/$(PROJ); ../config/revision-gen $(OpenACD_VER))
 OpenACD_SRPM = openacd-$(OpenACD_VER)-$(OpenACD_REV).src.rpm
 OpenACD_TAR = OpenACD/openacd-$(OpenACD_VER).tar.gz
 OpenACD_SRPM_DEFS = --define "buildno $(OpenACD_REV)"

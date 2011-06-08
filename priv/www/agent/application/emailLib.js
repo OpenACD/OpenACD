013c0969c2d42401bd72d6a1114727c548655d19
Micah Warren <micahw@lordnull.com>
2011-06-08 11:57:54 -0500

    Linted

--- priv/www/agent/application/emailLib.js
+++ priv/www/agent/application/emailLib.js
@@ -3,48 +3,28 @@ dojo.require("agentUI.util");
 
 dojo.provide("agentUI.emailLib");
 
-if(typeof(emailLib) == 'undefined'){
+if(typeof(emailLib) === 'undefined'){
 	emailLib = function(){
 		throw new Error("emailLib is a lib, and so can't be instantiated");
-	}
+	};
 	
 	dojo.create('script', {type:'text/javascript', src:'/html-sanitizer-minified.js'}, dojo.query('head')[0], 'last');
 	
 	emailLib.getSkeleton = function(){
-		dojo.xhrPost({
-			url:"/media",
-			handleAs:"json",
-			content:{
-				"command":"get_skeleton",
-				"arguments":[],
-				"mode":"call"
-			},
-			load:function(res){
-				dojo.publish("emailLib/get_skeleton", [res]);
-			},
-			error:function(res){
-				warning(["err getting email skeleton", res]);
+		// yay globals!
+		window.agentConnection.webApi('api', 'media_command', {
+			success:function(res){
+				dojo.publish('emailLib/get_skeleton', [res]);
 			}
-		});
+		}, 'get_skeleton', 'call');
 	};
 	
 	emailLib.getPath = function(path){
-		path = path.join("/");
-		dojo.xhrPost({
-			url:"/media",
-			content:{
-				"command":"get_path",
-				"arguments":path,
-				"mode":"call"
-			},
-			load:function(res){
-				debug(["load done", res, "emailLib/get_path/" + path]);
+		window.agentConnection.webApi('api', 'media_command', {
+			success:function(res){
 				dojo.publish("emailLib/get_path/" + path, [res]);
-			},
-			error:function(res){
-				warning(["err getting path", res]);
 			}
-		});
+		}, 'get_path', 'call', [path]);
 	};
 	
 	emailLib.pathsToFetch = function(skeleton, path, fetches){
@@ -57,38 +37,41 @@ if(typeof(emailLib) == 'undefined'){
 		
 		var copyPath = function(arr){
 			var out = [];
-			for(var i = 0; i < arr.length; i++){
+			var i;
+			for(i = 0; i < arr.length; i++){
 				out.push(arr[i]);
 			}
 			return out;
 		};
 		
 		debug(["pathsToFetch", skeleton, path]);
-		if( (skeleton.type == "multipart") && (skeleton.subtype == "alternative") ){
+		var tpath;
+		var i = 0;
+		if( (skeleton.type === "multipart") && (skeleton.subtype === "alternative") ){
 			var getting = 0;
 			var pushon = false;
 			var texttype = "";
-			for(var i = 0; i < skeleton.parts.length; i++){
-				if( (skeleton.parts[i].subtype == "plain") && (getting < 1) ){
+			for(i = 0; i < skeleton.parts.length; i++){
+				if( (skeleton.parts[i].subtype === "plain") && (getting < 1) ){
 					getting = 1;
 					pushon = i + 1;
 					texttype = 'plain';
 				}				
-				if( (skeleton.parts[i].subtype == "html") && ( getting < 2 ) ){
+				if( (skeleton.parts[i].subtype === "html") && ( getting < 2 ) ){
 					getting = 2;
 					pushon = i + 1;
 					texttype = 'html';
 				}
-				if( (skeleton.parts[i].type == "multipart") && (getting < 3) ){
+				if( (skeleton.parts[i].type === "multipart") && (getting < 3) ){
 					getting = 3;
 					pushon = i + 1;
 				}
 			}
 			
 			if(pushon){
-				var tpath = copyPath(path);
+				tpath = copyPath(path);
 				tpath.push(pushon);
-				if(getting == 3){
+				if(getting === 3){
 					fetches = emailLib.pathsToFetch(skeleton.parts[pushon - 1], tpath, fetches);
 				}
 				else{
@@ -104,7 +87,7 @@ if(typeof(emailLib) == 'undefined'){
 			return fetches;
 		}
 		
-		if( (skeleton.type == "multipart") ){
+		if( (skeleton.type === "multipart") ){
 			for(i = 0; i < skeleton.parts.length; i++){
 				tpath = copyPath(path);
 				tpath.push(i + 1);
@@ -114,7 +97,7 @@ if(typeof(emailLib) == 'undefined'){
 			return fetches;
 		}
 		
-		if(skeleton.type == "message" && skeleton.subtype == "delivery-status"){
+		if(skeleton.type === "message" && skeleton.subtype === "delivery-status"){
 			fetches.push({
 				'mode':'fetch',
 				'path':path,
@@ -123,7 +106,7 @@ if(typeof(emailLib) == 'undefined'){
 			return fetches;
 		}
 		
-		if(skeleton.type == "message"){
+		if(skeleton.type === "message"){
 			tpath = copyPath(path);
 			tpath.push(1);
 			fetches.push({
@@ -135,7 +118,7 @@ if(typeof(emailLib) == 'undefined'){
 			return fetches;
 		}
 		
-		if(skeleton.type == "text" && skeleton.subtype != "rtf"){
+		if(skeleton.type === "text" && skeleton.subtype !== "rtf"){
 			fetches.push({
 				'mode':'fetch',
 				'path':path,
@@ -144,7 +127,7 @@ if(typeof(emailLib) == 'undefined'){
 			return fetches;
 		}
 		
-		if(skeleton.type == "image"){
+		if(skeleton.type === "image"){
 			fetches.push({
 				'mode':'img',
 				'path':path
@@ -174,19 +157,19 @@ if(typeof(emailLib) == 'undefined'){
 		var jpath = fetchObjs[0].path.join("/");
 		
 		debug(["subbed to", "emailLib/get_path/" + fetchObjs[0].path.join("/")]);
-		if(fetchObjs[0].mode == 'a'){
+		if(fetchObjs[0].mode === 'a'){
 			fetched += '<a href="/' + jpath + '" target="_blank"><img src="/images/dl.png" style="border:none"/>' + fetchObjs[0].label + '</a>';
 		}
-		else if(fetchObjs[0].mode == 'img'){
+		else if(fetchObjs[0].mode === 'img'){
 			fetched += '<img src="/' + jpath + '" />';
 		}
-		else if(fetchObjs[0].mode == 'fetch'){
+		else if(fetchObjs[0].mode === 'fetch'){
 			emailLib.fetchSub = dojo.subscribe("emailLib/get_path/" + jpath, function(res){
 				debug(["sub hit", "emailLib/get_path/" + jpath, res]);
 				dojo.unsubscribe(emailLib.fetchSub);
 				emailLib.fetchSub = false;
 				if(fetchObjs[0].textType){
-					if(fetchObjs[0].textType == 'html'){
+					if(fetchObjs[0].textType === 'html'){
 						fetched += html_sanitize(res, emailLib.urlSanitize, emailLib.nameIdSanitize);
 					}else{
 						res = emailLib.scrubString(res).replace(/\n/g, '<br />');
@@ -218,10 +201,10 @@ if(typeof(emailLib) == 'undefined'){
 	
 	//scrub &, <, and > so it's displayable via html
 	emailLib.scrubString = function(instr){
-		if(instr == undefined){
+		if(instr === undefined){
 			return '';
 		}
-		return instr.replace(/\&/g, '&amp;').replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/\"/g, '&quot;'); //"
+		return instr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\"/g, '&quot;'); //"
 	};
 
 	emailLib.urlSanitize = function(url){
@@ -232,28 +215,28 @@ if(typeof(emailLib) == 'undefined'){
 		switch(l.protocol){
 			case 'cid:':
 				return escape(url);
-				break;
+				//break;
 			case 'mailto:':
 				return url;
-				break;
+				//break;
 			case 'http:':
-				if(	(l.hostname == window.location.hostname) && 
-					(l.port == window.location.port) ){
+				if(	(l.hostname === window.location.hostname) && 
+					(l.port === window.location.port) ){
 					return url;
 				} else {
 					l.protocol = 'scrub';
 				return l.href;
 				}
-				break;
+				//break;
 			default:
 				l.protocol = 'scrub';
 				return l.href;
 		}
-	}
+	};
 
 	emailLib.nameIdSanitize = function(name){
 		return "santizationPrefix-" + name;
-	}
+	};
 
 	emailLib.getFrom = function(callback){
 		dojo.xhrPost({
@@ -271,5 +254,5 @@ if(typeof(emailLib) == 'undefined'){
 				warning(["err getting from address", res]);
 			}
 		});
-	}
+	};
 }

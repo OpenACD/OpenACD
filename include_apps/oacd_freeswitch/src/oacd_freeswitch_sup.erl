4da7862444bf1663c7855d234649360470884bce
Micah Warren <micahw@lordnull.com>
2012-01-30 13:30:04 -0600

    Made start mirror old start

--- include_apps/oacd_freeswitch/src/oacd_freeswitch_sup.erl
+++ include_apps/oacd_freeswitch/src/oacd_freeswitch_sup.erl
@@ -2,9 +2,12 @@
 -behavior(supervisor).
 
 -export([init/1]).
+-export([start_link/2]).
 
-init(Args) ->
-	Node = proplists:get_value(node, Args),
-	MFA = {freeswitch_media_manager, start_link, [Node, Args]},
+start_link(FsNode, Args) ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, {FsNode, Args}).
+
+init({FsNode, Args}) ->
+	MFA = {freeswitch_media_manager, start_link, [FsNode, Args]},
 	Kid = {freeswitch_media_manager, MFA, permanent, 1000, worker, [freeswitch_media_manager]},
 	{ok, {{one_for_one, 5, 10}, [Kid]}}.

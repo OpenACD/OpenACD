4f2ba169c2bed7617aaac70f40758b5aa8a501fe
Micah Warren <micahw@lordnull.com>
2012-01-31 13:37:27 -0600

    Added merge to allow for config via app files.

--- include_apps/oacd_dummy/src/oacd_dummy_app.erl
+++ include_apps/oacd_dummy/src/oacd_dummy_app.erl
@@ -10,10 +10,31 @@
 %% ==================================================
 
 start(_Type, Args) ->
-	oacd_dummy_sup:start_link(Args).
+	?INFO("Starting dummy plugin:  ~p", [Args]),
+	Args0 = merge_env(Args),
+	oacd_dummy_sup:start_link(Args0).
 
 prep_stop(State) ->
 	State.
 
 stop(_State) ->
 	ok.
+
+merge_env(Args) ->
+	Env = application:get_all_env(oacd_dummy),
+	merge_env(lists:sort(Args), lists:sort(Env), []).
+
+merge_env([], Env, Acc) ->
+	lists:append(Acc, Env);
+
+merge_env(Args, [], Acc) ->
+	lists:append(Acc, Args);
+
+merge_env([{Key, Val} | ATail], [{Key, _} | ETail], Acc) ->
+	merge_env(ATail, ETail, [{Key, Val} | Acc]);
+
+merge_env([{Akey, Val} | ATail], [{EKey, _} | _] = Env, Acc) when Akey > EKey ->
+	merge_env(ATail, Env, [{Akey, Val} | Acc]);
+
+merge_env(Args, [Opt | ETail], Acc) ->
+	merge_env(Args, ETail, [Opt | Acc]).

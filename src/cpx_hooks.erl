fe133666043b7504a87f3d1424d4940ec8775818
Micah Warren <micahw@lordnull.com>
2012-01-31 15:57:04 -0600

    Updated docs to show hooks exist.

--- src/cpx_hooks.erl
+++ src/cpx_hooks.erl
@@ -1,3 +1,10 @@
+%% @doc Interface for plugins or modules to provide or take action on 
+%% some events.  When a hook is triggered, the list of callback functions
+%% set for that hook are called in order of priority from least to greatest.
+%% Processing stops at the first callback that returns `{ok, term()}'.  If
+%% no callbacks return `{ok, term()}', `{error, unhandled}' is returned.
+%% if a callback errors, it is removed from the list, never to be called
+%% again.  Callbacks are run in the same process that triggered the hook.
 -module(cpx_hooks).
 -behavior(gen_server).
 
@@ -18,20 +25,34 @@
 %% API
 %% =================================================================
 
+%% @doc Start up the process that manages the ets table.
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, {}, []).
 
+%% @doc Register a callback for a given hook with a priority of 100.
+%% @see set_hook/6
 set_hook(Id, Hook, {M, F, A}) ->
 	set_hook(Id, Hook, M, F, A, 100).
 
+%% @doc Register a callback for a given hook.  `Id' can be used to remove
+%% the hook at a later time.  If a hook with `Id' already exists, it is
+%% overwritten.  `M' is the module for the callback, `F' is the function
+%% of that module, and `A' is a list of additional args appened to the
+%% arguments passed in when the hook is triggered.  `Priority' determines
+%% the order callbacks are called in.  The lower the priority, the earlier
+%% the callback is called (as in 1st place, 2nd place, etc).
 set_hook(Id, Hook, M, F, A, Priority) when is_list(A) ->
 	ets:insert(cpx_hooks, {Id, Hook, M, F, A, Priority}).
 	%gen_server:cast(?MODULE, {set_hook, Id, Hook, M, F, A, Priority}).
 
+%% @doc Remove a callback with the given idea.
 drop_hook(Id) ->
 	ets:delete(cpx_hooks, Id).
 	%gen_server:cast(?MODULE, {drop_hook, Id}).
 
+%% @doc Trigger a hook, passing the the given args along.  Callbacks are
+%% run in the calling process.  Generally a bad idea to trigger a hook
+%% specified outside your application.
 trigger_hooks(Hook, Args) ->
 	Hooks = qlc:e(qlc:q([{P, M, F, A, Id} || 
 		{Id, EHook, M, F, A, P} <- ets:table(cpx_hooks),
@@ -40,6 +61,8 @@ trigger_hooks(Hook, Args) ->
 	Hooks0 = lists:sort(Hooks),
 	run_hooks(Hooks0, Args).
 
+%% @doc Stop the hook server, thus removing the ets holding callback
+%% information.
 stop() ->
 	gen_server:call(?MODULE, stop).
 
@@ -51,6 +74,7 @@ stop() ->
 %% init
 %% -----------------------------------------------------------------
 
+%% @private
 init(_) ->
 	Ets = ets:new(cpx_hooks, [named_table, public]),
 	{ok, Ets}.
@@ -59,9 +83,11 @@ init(_) ->
 %% handle_call
 %% -----------------------------------------------------------------
 
+%% @private
 handle_call(stop, _, Ets) ->
 	{stop, normal, ok, Ets};
 
+%% @private
 handle_call(_, _, Ets) ->
 	{reply, invalid, Ets}.
 
@@ -70,11 +96,13 @@ handle_call(_, _, Ets) ->
 %% handle_cast
 %% -----------------------------------------------------------------
 
+%% @private
 handle_cast({set_hook, Id, Hook, M, F, A, Priority} = H, Ets) ->
 	?DEBUG("Setting ~p", [H]),
 	ets:insert(Ets, {Id, Hook, M, F, A, Priority}),
 	{noreply, Ets};
 
+%% @private
 handle_cast({drop_hook, Id}, Ets) ->
 	ets:delete(Ets, Id),
 	{noreply, Ets}.
@@ -84,6 +112,7 @@ handle_cast({drop_hook, Id}, Ets) ->
 %% handle_info
 %% -----------------------------------------------------------------
 
+%% @private
 handle_info(_, Ets) ->
 	{noreply, Ets}.
 
@@ -92,6 +121,7 @@ handle_info(_, Ets) ->
 %% terminate
 %% -----------------------------------------------------------------
 
+%% @private
 terminate(_Reason, Ets) ->
 	Ets.
 
@@ -99,6 +129,7 @@ terminate(_Reason, Ets) ->
 %% code_change
 %% -----------------------------------------------------------------
 
+%% @private
 code_change(_OldVsn, Ets, _Extra) ->
 	{ok, Ets}.
 
@@ -106,6 +137,7 @@ code_change(_OldVsn, Ets, _Extra) ->
 %% internal
 %% =================================================================
 
+%% @private
 run_hooks([], _Args) ->
 	{error, unhandled};
 

e11814f6b4c78192007094fb06f2533890ac0bb9
Micah Warren <micahw@lordnull.com>
2012-02-01 15:34:05 -0600

    Added actions to take as though this were a dep.
    Rebar doesn't change working directories or add lib dirs from dependencies.
    The changes to the pre_get-deps will work to put the include_apps in the
    correct place as well as the OpenACD protobuffs.  This will likely not
    work as expected if not used as a dependecy.

--- pre_get-deps.sh
+++ pre_get-deps.sh
@@ -4,3 +4,15 @@ if [ "${GIT_UPDATE_DISABLED}" != "1" ]; then
 	echo "Updating submodules..."
 	git submodule init && git submodule update
 fi
+
+if [ ! -d ../oacd_freeswitch ]; then
+	if [ ! -d ../oacd_dummy ]; then
+		cp -R include_apps/ ../
+	fi
+fi
+
+if [ ! -d ../../proto_src ]; then
+	mkdir ../../proto_src
+fi
+
+cp proto_src/*.proto ../../proto_src

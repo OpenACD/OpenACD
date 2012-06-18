3c11140b8aa0d5a2eaad68735108a2ce26526333
Micah Warren <micahw@lordnull.com>
2012-06-18 11:36:22 -0500

    Updated cdr tests.

--- src/cdr.erl
+++ src/cdr.erl
@@ -1011,6 +1011,7 @@ push_raw_test_() ->
 			{ringing, "testagent"},
 			{precall, "na"},
 			{oncall, "testagent"},
+			{oncall_transition, "testagent"},
 			{failedoutgoing, "na"},
 			{agent_transfer, "na"},
 			{queue_transfer, "na"},
@@ -1104,6 +1105,14 @@ push_raw_test_() ->
 		fun() ->
 			push_raw(Call, #cdr_raw{id = Call#call.id, transaction = oncall, eventdata = "testagent"}),
 			Testend = [inqueue, ringing, precall, warmxfer_cancel, dialoutgoing],
+			Ended(Pull(), Testend)
+		end}
+	end,
+	fun({Call, Pull, Ended}) ->
+		{"oncall_transition",
+		fun() ->
+			push_raw(Call, #cdr_raw{id = Call#call.id, transaction = oncall_transition, eventdata = "testagent"}),
+			Testend = [],
 			Ended(Pull(), Testend)
 		end}
 	end,
@@ -1238,6 +1247,7 @@ push_raw_test_() ->
 				ringing,
 				precall,
 				oncall,
+				oncall_transition,
 				failedoutgoing,
 				agent_transfer,
 				queue_transfer,

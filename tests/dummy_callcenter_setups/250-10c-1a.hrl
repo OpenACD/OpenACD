bc7e4593b41c4171a03c180df403f2a6eccc9726
Micah Warren <micahw@lordnull.com>
2012-07-16 16:15:42 -0500

    Adding dummy_callcenter sets for load.

---
+++ tests/dummy_callcenter_setups/250-10c-1a.hrl
@@ -0,0 +1,8 @@
+{start_count, 250}.
+{call_frequency, 1}.
+{agents, 25}.
+{agent_opts, [
+	{ringing, 3},
+	{oncall, 6},
+	{wrapup, 3}
+]}.
---
+++ tests/dummy_callcenter_setups/250-both.hrl
@@ -0,0 +1,8 @@
+{start_count, 250}.
+{call_frequency, 1}.
+{agents, 250}.
+{agent_opts, [
+	{ringing, 3},
+	{oncall, 6},
+	{wrapup, 3}
+]}.
